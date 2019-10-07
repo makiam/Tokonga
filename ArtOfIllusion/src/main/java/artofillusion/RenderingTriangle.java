83ccc10dd8df9ab646413f85399fc24c4b11c393
Maksim Khramov <maxtemplaar@gmail.com>
2019-10-07 15:16:26 +0300

    Eliminate raw types

--- ArtOfIllusion/src/main/java/artofillusion/RenderingTriangle.java
+++ ArtOfIllusion/src/main/java/artofillusion/RenderingTriangle.java
@@ -25,7 +25,7 @@ public abstract class RenderingTriangle implements Cloneable {
     public RenderingMesh theMesh;
 
     private static double EMPTY_ARRAY[] = new double[0];
-    protected static ThreadLocal<double[]> tempParamValues = new ThreadLocal(); // Used when rendering layered textures
+    protected static ThreadLocal<double[]> tempParamValues = new ThreadLocal<>(); // Used when rendering layered textures
 
     public RenderingTriangle(int v1, int v2, int v3, int n1, int n2, int n3) {
         this.v1 = v1;
