3fc5f1d362ae8432efd658d98948e11bb35a0dc4
Maksim Khramov <maxtemplaar@gmail.com>
2019-10-23 17:33:21 +0300

    Update plugin to use Default Implementation

--- Polymesh/src/main/java/artofillusion/polymesh/PolyMeshPlugin.java
+++ Polymesh/src/main/java/artofillusion/polymesh/PolyMeshPlugin.java
@@ -1,6 +1,6 @@
 /*
  *  Copyright 2004-2007 Francois Guillet
-    Changes copyright (C) 2017 by Maksim Khramov
+    Changes copyright (C) 2017-2019 by Maksim Khramov
 
  *  This program is free software; you can redistribute it and/or modify it under the
  *  terms of the GNU General Public License as published by the Free Software
@@ -11,123 +11,99 @@
  */
 package artofillusion.polymesh;
 
-import java.io.InputStream;
-import java.util.ResourceBundle;
 import artofillusion.ArtOfIllusion;
+import artofillusion.DefaultPluginImplementation;
 import artofillusion.LayoutWindow;
-import artofillusion.Plugin;
 import artofillusion.UndoRecord;
 import artofillusion.keystroke.KeystrokeManager;
 import artofillusion.keystroke.KeystrokeRecord;
-import artofillusion.math.CoordinateSystem;
 import artofillusion.object.ObjectInfo;
 import artofillusion.object.SplineMesh;
 import artofillusion.object.TriangleMesh;
-import artofillusion.ui.ToolPalette;
 import artofillusion.ui.Translate;
-import buoy.widget.BMenu;
+import buoy.event.CommandEvent;
 import buoy.widget.BMenuItem;
 import buoy.widget.BStandardDialog;
-import buoy.widget.MenuWidget;
+import com.google.common.flogger.FluentLogger;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import javax.swing.SwingUtilities;
 
 /**
  * This is the plugin class that plugs PolyMesh structure and editing features into AoI
  *
  * @author Francois Guillet
  */
-public class PolyMeshPlugin implements Plugin {
+public class PolyMeshPlugin extends DefaultPluginImplementation {
+
+    private static final com.google.common.flogger.FluentLogger logger = FluentLogger.forEnclosingClass();
 
     public static ResourceBundle resources;
 
-    /**
-     * Process messages sent to plugin by AoI (see AoI API description)
-     *
-     * @param message The message
-     * @param args Arguments depending on the message
-     */
     @Override
-    public void processMessage(int message, Object args[]) {
-        if (message == Plugin.APPLICATION_STARTING) {
-            resources = ResourceBundle.getBundle("polymesh", ArtOfIllusion.getPreferences().getLocale());
-            boolean keysImplemented = false;
-            for (KeystrokeRecord key : KeystrokeManager.getAllRecords()) {
-                if (key.getName().endsWith("(PolyMesh)")) {
-                    keysImplemented = true;
-                    break;
-                }
-            }
-            if (keysImplemented) {
-                return;
-            }
+    protected void onSceneWindowCreated(LayoutWindow view) {
 
-            try {
-                InputStream in = getClass().getResourceAsStream("/PMkeystrokes.xml");
+        BMenuItem convertMenuItem = Translate.menuItem("polymesh:convertToPolyMesh", this, "convertToPolymeshMenuAction");
+        convertMenuItem.getComponent().putClientProperty("layout", view);
+
+        SwingUtilities.invokeLater(() -> {
+            view.getToolPalette().addTool(8, new CreatePolyMeshTool(view));
+            view.getObjectMenu().add(convertMenuItem, 7);
+            view.layoutChildren();
+        });
+
+    }
+
+    @Override
+    protected void onApplicationStarting() {
+        resources = ResourceBundle.getBundle("polymesh", ArtOfIllusion.getPreferences().getLocale());
+        boolean keysImplemented = false;
+        for (KeystrokeRecord key : KeystrokeManager.getAllRecords()) {
+            if (key.getName().endsWith("(PolyMesh)")) {
+                keysImplemented = true;
+                break;
+            }
+        }
+        if (keysImplemented) {
+            return;
+        }
+
+        try {
+            try (InputStream in = getClass().getResourceAsStream("/PMkeystrokes.xml")) {
                 KeystrokeManager.addRecordsFromXML(in);
-                in.close();
-                KeystrokeManager.saveRecords();
-            } catch (Exception ex) {
-                ex.printStackTrace();
             }
-
-        } else if (message == Plugin.SCENE_WINDOW_CREATED) {
-            LayoutWindow layout = (LayoutWindow) args[0];
-            ToolPalette palette = layout.getToolPalette();
-            palette.addTool(8, new CreatePolyMeshTool(layout));
-            palette.toggleDefaultTool();
-            palette.toggleDefaultTool();
-            BMenuItem menuItem = Translate.menuItem("polymesh:convertToPolyMesh", new ConvertObject(layout), "doConvert");
-            BMenu toolsMenu = layout.getObjectMenu();
-            int count = toolsMenu.getChildCount();
-            MenuWidget[] mw = new MenuWidget[count];
-            for (int i = count - 1; i >= 0; i--) {
-                mw[i] = toolsMenu.getChild(i);
-            }
-            toolsMenu.removeAll();
-            for (int i = 0; i <= 7; i++) {
-                toolsMenu.add(mw[i]);
-            }
-            toolsMenu.add(menuItem);
-            for (int i = 8; i < count; i++) {
-                toolsMenu.add(mw[i]);
-            }
-            layout.layoutChildren();
+            KeystrokeManager.saveRecords();
+        } catch (Exception ex) {
+            logger.at(Level.SEVERE).withCause(ex).log("Unable to load keystroke records");
         }
     }
 
-    private class ConvertObject {
 
-        private final LayoutWindow window;
+    @SuppressWarnings("unused")
+    private void convertToPolymeshMenuAction(CommandEvent event) {
+        BMenuItem mi = (BMenuItem) event.getWidget();
+        LayoutWindow view = (LayoutWindow) mi.getComponent().getClientProperty("layout");
 
-        public ConvertObject(LayoutWindow window) {
-            this.window = window;
-        }
+        Collection<ObjectInfo> selection = view.getSelectedObjects();
 
-        private void doConvert() {
-            PolyMesh mesh;
-            BStandardDialog dlg = new BStandardDialog(Translate.text("polymesh:triangleToPolyTitle"), Translate.text("polymesh:convertToQuads"), BStandardDialog.QUESTION);
-            String[] options = new String[]{Translate.text("polymesh:findQuadsDistance"), Translate.text("polymesh:findQuadsAngular"), Translate.text("polymesh:keepTriangles")};
+        if(selection.isEmpty()) return;
 
-            //NB!!! optionDefault is not match to any option button defined above... 
-            String optionDefault = Translate.text("polymesh:convertToQuads");
+        selection.stream().filter((ObjectInfo test) -> test.getObject() instanceof SplineMesh).forEach((ObjectInfo item) -> {
+            view.addObject(new ObjectInfo(new PolyMesh((SplineMesh)item.getObject()), item.getCoords().duplicate(), "Polymesh" + item.getName()), (UndoRecord)null);
+        });
 
-            for (ObjectInfo item : window.getSelectedObjects()) {
-                if (item.getObject() instanceof SplineMesh) {
-                    mesh = new PolyMesh((SplineMesh) item.getObject());
-                    CoordinateSystem coords = new CoordinateSystem();
-                    coords.copyCoords(item.getCoords());
-                    String name = "Polymesh" + item.getName();
-                    window.addObject(mesh, coords, name, (UndoRecord) null);
-                } else if (item.getObject() instanceof TriangleMesh) {
-                    int r = dlg.showOptionDialog(window, options, optionDefault);
-                    mesh = new PolyMesh((TriangleMesh) item.getObject(), r == 0 || r == 1, r == 1);
-                    CoordinateSystem coords = new CoordinateSystem();
-                    coords.copyCoords(item.getCoords());
-                    String name = "Polymesh" + item.getName();
-                    window.addObject(mesh, coords, name, (UndoRecord) null);
-                }
-            }
+        BStandardDialog dlg = new BStandardDialog(Translate.text("polymesh:triangleToPolyTitle"), Translate.text("polymesh:convertToQuads"), BStandardDialog.QUESTION);
+        String[] options = new String[]{Translate.text("polymesh:findQuadsDistance"), Translate.text("polymesh:findQuadsAngular"), Translate.text("polymesh:keepTriangles")};
 
-            window.updateImage();
-        }
+        //NB!!! optionDefault is not match to any option button defined above...
+        String optionDefault = Translate.text("polymesh:convertToQuads");
+
+        selection.stream().filter((ObjectInfo test) -> test.getObject() instanceof TriangleMesh).forEach((ObjectInfo item) -> {
+            int result = dlg.showOptionDialog(view, options, optionDefault);
+            PolyMesh mesh = new PolyMesh((TriangleMesh) item.getObject(), result == 0 || result == 1, result == 1);
+            view.addObject(new ObjectInfo(mesh, item.getCoords().duplicate(), "Polymesh" + item.getName()), (UndoRecord)null);
+        });
     }
 }
