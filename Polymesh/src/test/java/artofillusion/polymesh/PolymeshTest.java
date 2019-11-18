374956540ad41eaeb603570cbf2078e934d10ff7
Maksim Khramov <maxtemplaar@gmail.com>
2019-11-18 18:19:48 +0300

    Polymesh Testcase

---
+++ Polymesh/src/test/java/artofillusion/polymesh/PolymeshTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package artofillusion.polymesh;
+
+import artofillusion.ArtOfIllusion;
+import artofillusion.ViewerCanvas;
+import artofillusion.math.RGBColor;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URISyntaxException;
+import java.util.Locale;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.netbeans.jemmy.ClassReference;
+import org.netbeans.jemmy.JemmyProperties;
+import org.netbeans.jemmy.TestOut;
+
+/**
+ *
+ * @author maksim.khramov
+ */
+public class PolymeshTest {
+    private static enum MeshType {
+        
+        CUBE(0),
+        OCTAHEDRON(2),
+        CYLINDER(3),
+        PLANE(4);
+        
+        private int type;
+        MeshType(int type) {
+            this.type =  type;
+        }
+        
+        
+    }
+    
+    @BeforeClass
+    public static void setupClass() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, URISyntaxException, IOException {
+        ViewerCanvas.lineColor = java.awt.Color.PINK;
+        ViewerCanvas.highlightColor = java.awt.Color.GREEN;
+        ViewerCanvas.transparentColor = new RGBColor();
+    }
+    
+    
+    @Test
+    public void testCreatePolyMeshCube() {
+        PolyMesh pm = new PolyMesh(MeshType.CUBE.type, 0, 0, 0, 1, 1);
+        
+        Assert.assertEquals(8, pm.getVertices().length);
+    }
+}
