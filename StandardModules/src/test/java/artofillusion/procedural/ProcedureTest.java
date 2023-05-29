c9a3795db1181aa010ef607ac2a9ec7be67e9dba
Maksim Khramov <maxtemplaar@gmail.com>
2023-05-29 23:13:20 +0300

    Update test to reduce Module reference usages

--- StandardModules/src/test/java/artofillusion/procedural/ProcedureTest.java
+++ StandardModules/src/test/java/artofillusion/procedural/ProcedureTest.java
@@ -79,14 +79,14 @@ public class ProcedureTest {
 
     @Test
     public void testGetMissedModuleFromProcedure() {
-        Module mod = new Module("Test", new IOPort[]{}, new IOPort[]{}, new Point());
+        var mod = new Module("Test", new IOPort[]{}, new IOPort[]{}, new Point());
         Procedure procedure = new Procedure(new OutputModule[]{});
         assertEquals(-1, procedure.getModuleIndex(mod));
     }
 
     @Test
     public void testGetSingleModuleIndex() {
-        Module mod = new Module("Test", new IOPort[]{}, new IOPort[]{}, new Point());
+        var mod = new Module("Test", new IOPort[]{}, new IOPort[]{}, new Point());
         Procedure procedure = new Procedure(new OutputModule[]{});
         procedure.addModule(mod);
         assertEquals(0, procedure.getModuleIndex(mod));
@@ -94,8 +94,8 @@ public class ProcedureTest {
 
     @Test
     public void testGetDoubleModuleIndex() {
-        Module mod1 = new Module("Test1", new IOPort[]{}, new IOPort[]{}, new Point());
-        Module mod2 = new Module("Test2", new IOPort[]{}, new IOPort[]{}, new Point());
+        var mod1 = new Module("Test1", new IOPort[]{}, new IOPort[]{}, new Point());
+        var mod2 = new Module("Test2", new IOPort[]{}, new IOPort[]{}, new Point());
         Procedure procedure = new Procedure(new OutputModule[]{});
         procedure.addModule(mod1);
         procedure.addModule(mod2);
@@ -105,8 +105,8 @@ public class ProcedureTest {
 
     @Test
     public void testDeleteFirstOfTwoModules() {
-        Module mod1 = new Module("Test1", new IOPort[]{}, new IOPort[]{}, new Point());
-        Module mod2 = new Module("Test2", new IOPort[]{}, new IOPort[]{}, new Point());
+        var mod1 = new Module("Test1", new IOPort[]{}, new IOPort[]{}, new Point());
+        var mod2 = new Module("Test2", new IOPort[]{}, new IOPort[]{}, new Point());
         Procedure procedure = new Procedure(new OutputModule[]{});
         procedure.addModule(mod1);
         procedure.addModule(mod2);
@@ -117,7 +117,7 @@ public class ProcedureTest {
 
     @Test
     public void testDeleteLastSingleModule() {
-        Module mod = new Module("Test", new IOPort[]{}, new IOPort[]{}, new Point());
+        var mod = new Module("Test", new IOPort[]{}, new IOPort[]{}, new Point());
         Procedure procedure = new Procedure(new OutputModule[]{});
         procedure.addModule(mod);
 
@@ -126,7 +126,7 @@ public class ProcedureTest {
 
     @Test(expected = ArrayIndexOutOfBoundsException.class)
     public void testDeleteMissedModule() {
-        Module mod = new Module("Test", new IOPort[]{}, new IOPort[]{}, new Point());
+        var mod = new Module("Test", new IOPort[]{}, new IOPort[]{}, new Point());
         Procedure procedure = new Procedure(new OutputModule[]{});
         procedure.addModule(mod);
 
@@ -136,7 +136,7 @@ public class ProcedureTest {
     @Test
     public void testInitModuleFromPoint() {
 
-        Module mod = new Module("Test", new IOPort[]{}, new IOPort[]{}, new Point()) {
+        var mod = new Module("Test", new IOPort[]{}, new IOPort[]{}, new Point()) {
             private PointInfo point;
 
             @Override
