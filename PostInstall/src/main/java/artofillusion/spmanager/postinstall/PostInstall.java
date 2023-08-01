b7d041bc418c7ed9778cf362473fa707caebc54e
Maksim Khramov <khramov.WX1235557@huawei-partners.com>
2023-08-01 13:08:04 +0300

    Clean PostInstall plugin

--- PostInstall/src/main/java/artofillusion/spmanager/postinstall/PostInstall.java
+++ PostInstall/src/main/java/artofillusion/spmanager/postinstall/PostInstall.java
@@ -28,8 +28,11 @@ import artofillusion.*;
 import artofillusion.ui.*;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.util.*;
+
 import lombok.extern.slf4j.Slf4j;
+
 import javax.swing.*;
 
 /**
@@ -38,155 +41,159 @@ import javax.swing.*;
 @Slf4j
 public class PostInstall implements Plugin {
 
-    protected List<String> ok, err;
-    protected static File tempDir = null;
+    private final List<String> ok = new ArrayList<>();
+    private final List<String> err = new ArrayList<>();
+
+    private static File tempDir = null;
+
+    @Override
+    public void onSceneWindowCreated(LayoutWindow view) {
+        if (!err.isEmpty()) {
+            JTextArea txt = new JTextArea();
+            txt.setColumns(45);
+            txt.setEditable(false);
+            txt.setRows(5);
+            txt.setText(String.join("\n", err));
+
+            JScrollPane detail = new JScrollPane(txt, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+            JLabel message = new JLabel(Translate.text("postinstall:errMsg"));
+
+            MessageDialog.create().withTitle("PostInstall").info(new JComponent[]{message, detail});
+
+        }
+
+        if (!ok.isEmpty()) {
+
+            JTextArea txt = new JTextArea();
+            txt.setColumns(45);
+            txt.setEditable(false);
+            txt.setRows(5);
+            txt.setText(String.join("\n", ok));
+
+            JScrollPane detail = new JScrollPane(txt, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+            JLabel message = new JLabel(Translate.text("postinstall:okMsg"));
+            JLabel restart = new JLabel(Translate.text("postinstall:restartMsg"));
+
+            MessageDialog.create().withTitle("PostInstall").info(new JComponent[]{message, restart, detail});
+        }
+
+        ok.clear();
+        err.clear();
+    }
 
     @Override
     public void processMessage(int msg, Object... args) {
-        int i, k;
 
         switch (msg) {
             case Plugin.APPLICATION_STARTING:
-	    try {
-
-                // find the SPManager temp dir
-                if (tempDir == null) {
-                    tempDir = new File(System.getProperty("java.io.tmpdir"));
-                }
-
-                if (!tempDir.exists()) {
-                    tempDir = new File(ArtOfIllusion.APP_DIRECTORY, "temp");
-                }
-
-                if (!tempDir.exists()) {
-                    tempDir = new File(System.getProperty("user.dir"), "SPMtemp");
-                }
-
-                if (!tempDir.exists()) {
-                    log.info("PostInstall: No TEMP dir found");
-
-                    tempDir = null;
-                    return;
-                }
-
-                // get the correct subtree
-                String prefix = "spmanager-temp-" + System.getProperty("user.name") + "-";
-
-                for (String sub : tempDir.list()) {
-                    if (sub.startsWith(prefix) && (!sub.endsWith(".lck"))) {
-                        File lockfile = new File(tempDir, sub + ".lck");
-
-                        // no lock-file means not active
-                        if (!lockfile.exists()) {
-                            tempDir = new File(tempDir, sub);
-                            break;
-                        }
-                    }
-                }
-
-                // if no subtree found, exit now
-                if (!tempDir.getName().startsWith("spmanager-temp-")) {
-                    log.info("PostInstall: no TEMP sub-tree found");
-
-                    tempDir = null;
-                    return;
-                }
-
-                log.info("PostInstall: tempDir is {}", tempDir.getAbsolutePath());
-
-                ok = new ArrayList<>(128);
-                err = new ArrayList<>(128);
-
-                cleanup(ArtOfIllusion.PLUGIN_DIRECTORY, ok, err);
-                cleanup(ArtOfIllusion.TOOL_SCRIPT_DIRECTORY, ok, err);
-                cleanup(ArtOfIllusion.OBJECT_SCRIPT_DIRECTORY, ok, err);
-                cleanup(ArtOfIllusion.STARTUP_SCRIPT_DIRECTORY, ok, err);
-            } catch (Exception e) {
-                log.atError().setCause(e).log("PostInstall: exception raised - aborting: {}", e.getMessage());
-                err.add("Exception raised - aborting: " + e);
-            } finally {
-
-                if (tempDir == null) {
-                    return;
-                }
-
-                // delete the temp tree
                 try {
-                    File tmp;
-                    String[] sub = tempDir.list();
-                    List<String> list = new ArrayList<>(sub.length);
-                    for (i = 0; i < sub.length; i++) {
-                        list.add(tempDir + File.separator + sub[i]);
+
+                    // find the SPManager temp dir
+                    if (tempDir == null) {
+                        tempDir = new File(System.getProperty("java.io.tmpdir"));
                     }
 
-                    for (i = 0; i < list.size(); i++) {
-                        tmp = new File(list.get(i));
+                    if (!tempDir.exists()) {
+                        tempDir = new File(ArtOfIllusion.APP_DIRECTORY, "temp");
+                    }
 
-                        // make sure we empty all sub-directories first
-                        if (tmp.isDirectory()) {
-                            sub = tmp.list();
-                            if (sub.length > 0) {
-                                log.info("PostInstall: descending into {}", tmp.getAbsolutePath());
+                    if (!tempDir.exists()) {
+                        tempDir = new File(System.getProperty("user.dir"), "SPMtemp");
+                    }
 
-                                for (k = 0; k < sub.length; k++) {
-                                    list.add(i, tmp.getAbsolutePath() + File.separator + sub[k]);
-                                }
+                    if (!tempDir.exists()) {
+                        log.info("PostInstall: No TEMP dir found");
 
-                                // continue processing from this element
-                                i--;
-                                continue;
+                        tempDir = null;
+                        return;
+                    }
+
+                    // get the correct subtree
+                    String prefix = "spmanager-temp-" + System.getProperty("user.name") + "-";
+
+                    for (String sub : tempDir.list()) {
+                        if (sub.startsWith(prefix) && (!sub.endsWith(".lck"))) {
+                            File lockfile = new File(tempDir, sub + ".lck");
+
+                            // no lock-file means not active
+                            if (!lockfile.exists()) {
+                                tempDir = new File(tempDir, sub);
+                                break;
                             }
                         }
-                        log.info("PostInstall: deleting {}", tmp.getAbsolutePath());
-
-                        tmp.delete();
                     }
+
+                    // if no subtree found, exit now
+                    if (!tempDir.getName().startsWith("spmanager-temp-")) {
+                        log.info("PostInstall: no TEMP sub-tree found");
+
+                        tempDir = null;
+                        return;
+                    }
+
+                    log.info("PostInstall: tempDir is {}", tempDir.getAbsolutePath());
+
+
+                    cleanup(ArtOfIllusion.PLUGIN_DIRECTORY, ok, err);
+                    cleanup(ArtOfIllusion.TOOL_SCRIPT_DIRECTORY, ok, err);
+                    cleanup(ArtOfIllusion.OBJECT_SCRIPT_DIRECTORY, ok, err);
+                    cleanup(ArtOfIllusion.STARTUP_SCRIPT_DIRECTORY, ok, err);
                 } catch (Exception e) {
-                    log.atError().setCause(e).log("PostInstall error: {}", e.getMessage());
+                    log.atError().setCause(e).log("PostInstall: exception raised - aborting: {}", e.getMessage());
+                    err.add("Exception raised - aborting: " + e);
+                } finally {
+
+                    if (tempDir == null) {
+                        return;
+                    }
+
+                    // delete the temp tree
+                    try {
+                        File tmp;
+                        String[] sub = tempDir.list();
+                        List<String> list = new ArrayList<>(sub.length);
+                        int i;
+                        for (i = 0; i < sub.length; i++) {
+                            list.add(tempDir + File.separator + sub[i]);
+                        }
+
+                        for (i = 0; i < list.size(); i++) {
+                            tmp = new File(list.get(i));
+
+                            // make sure we empty all subdirectories first
+                            if (tmp.isDirectory()) {
+                                sub = tmp.list();
+                                if (sub.length > 0) {
+                                    log.info("PostInstall: descending into {}", tmp.getAbsolutePath());
+
+                                    for (String s : sub) {
+                                        list.add(i, tmp.getAbsolutePath() + File.separator + s);
+                                    }
+
+                                    // continue processing from this element
+                                    i--;
+                                    continue;
+                                }
+                            }
+                            log.info("PostInstall: deleting {}", tmp.getAbsolutePath());
+
+                            tmp.delete();
+                        }
+                    } catch (Exception e) {
+                        log.atError().setCause(e).log("PostInstall error: {}", e.getMessage());
+                    }
+
+                    log.info("PostInstall: deleting {}", tempDir.getAbsolutePath());
+
+                    tempDir.delete();
                 }
-
-                log.info("PostInstall: deleting {}", tempDir.getAbsolutePath());
-
-                tempDir.delete();
-            }
-            break;
+                break;
 
             case Plugin.SCENE_WINDOW_CREATED:
-	    try {
-                if (err != null && err.size() > 0) {
-                    JTextArea txt = new JTextArea();
-                    txt.setColumns(45);
-                    txt.setEditable(false);
-                    txt.setRows(5);
-                    txt.setText(String.join("\n", err));
-
-                    JScrollPane detail = new JScrollPane(txt, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-
-                    JLabel message = new JLabel(Translate.text("postinstall:errMsg"));
-
-                    MessageDialog.create().withTitle("PostInstall").info(new JComponent[]{ message, detail });
-
-                }
-
-                if (ok != null && ok.size() > 0) {
-
-                    JTextArea txt = new JTextArea();
-                    txt.setColumns(45);
-                    txt.setEditable(false);
-                    txt.setRows(5);
-                    txt.setText(String.join("\n", ok));
-
-                    JScrollPane detail = new JScrollPane(txt, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-
-                    JLabel message = new JLabel(Translate.text("postinstall:okMsg"));
-                    JLabel restart = new JLabel(Translate.text("postinstall:restartMsg"));
-
-                    MessageDialog.create().withTitle("PostInstall").info(new JComponent[]{ message, restart, detail });
-                }
-            } finally {
-                ok = null;
-                err = null;
-            }
+                onSceneWindowCreated((LayoutWindow) args[0]);
+                break;
         }
     }
 
@@ -197,8 +204,6 @@ public class PostInstall implements Plugin {
      */
     public static void cleanup(String path, List<String> ok, List<String> err) {
         File from, to;
-        File update;
-        String fname;
 
         to = new File(path);
         from = new File(tempDir, to.getName());
@@ -219,12 +224,12 @@ public class PostInstall implements Plugin {
         }
 
         // iterate all filenames in the directory
-        for (int i = 0; i < files.length; i++) {
+        for (String file : files) {
 
             // only process filename that look like an update file
-            if (files[i].endsWith(".upd")) {
+            if (file.endsWith(".upd")) {
 
-                update = new File(from, files[i]);
+                File update = new File(from, file);
 
                 // if the file is zero-length, jut try to delete it
                 if (update.length() == 0) {
@@ -236,12 +241,12 @@ public class PostInstall implements Plugin {
                         err.add("could not delete " + update.getAbsolutePath());
                     }
 
-                    continue;	// skip to next file
+                    continue;    // skip to next file
                 }
 
-                fname = files[i].substring(0, files[i].length() - ".upd".length());
+                String fileName = file.substring(0, file.length() - ".upd".length());
 
-                File plugin = new File(to, fname);
+                File plugin = new File(to, fileName);
 
                 // if the corresponding plugin also exists then fix it
                 if (plugin.exists()) {
@@ -249,34 +254,18 @@ public class PostInstall implements Plugin {
                 }
 
                 if (update.renameTo(plugin)) {
-                    log.info("PostInstall.cleanup: Updated {}", fname);
-                    ok.add("Updated " + fname);
+                    log.info("PostInstall.cleanup: Updated {}", fileName);
+                    ok.add("Updated " + fileName);
                 } else {
-                    InputStream is = null;
-                    OutputStream os = null;
                     try {
-                        is = new BufferedInputStream(new FileInputStream(update));
-                        os = new BufferedOutputStream(new FileOutputStream(plugin));
+                        Files.copy(update.toPath(), plugin.toPath());
 
-                        int b;
-                        while ((b = is.read()) >= 0) {
-                            os.write((byte) b);
-                        }
-
-                        log.info("PostInstall.cleanup: Updated {}", fname);
-
-                        ok.add("Updated (copied) " + fname);
+                        log.info("PostInstall.cleanup: Updated {}", fileName);
+                        ok.add("Updated (copied) " + fileName);
                     } catch (IOException e) {
-                        log.atError().setCause(e).log("PostInstall.cleanup: **Error updating ", fname);
-                        err.add("couldn't rename or copy " + fname);
+                        log.atError().setCause(e).log("PostInstall.cleanup: **Error updating {}", fileName);
+                        err.add("couldn't rename or copy " + fileName);
                     } finally {
-                        try {
-                            os.flush();
-                            os.close();
-                            is.close();
-                        } catch (IOException e) {
-                        }
-
                         if (!update.delete()) {
                             log.atError().log("PostInstall.cleanup: **Error: Could not delete: {}", update.getAbsolutePath());
 
