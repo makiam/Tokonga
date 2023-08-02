6f706f0bb22920c904f313cffccf96719dd76291
Maksim Khramov <maxtemplaar@gmail.com>
2023-08-03 00:23:19 +0300

    Preferences plugin stub

---
+++ Preferences/src/main/java/artofillusion/preferences/PreferencesPlugin.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package artofillusion.preferences;
+import artofillusion.Plugin;
+import lombok.extern.slf4j.Slf4j;
+
+
+
+/**
+ *
+ * @author MaksK
+ */
+@Slf4j
+public class PreferencesPlugin implements Plugin {
+
+}
