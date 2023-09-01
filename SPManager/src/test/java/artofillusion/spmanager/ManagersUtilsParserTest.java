189adee20b7869cfd7dfe599208d0cfe0a598e34
Maksim Khramov <khramov.WX1235557@huawei-partners.com>
2023-09-01 12:28:29 +0300

    WIP: Update SPM to use Preferences plugin

--- SPManager/src/test/java/artofillusion/spmanager/ManagersUtilsParserTest.java
+++ SPManager/src/test/java/artofillusion/spmanager/ManagersUtilsParserTest.java
@@ -1,5 +1,6 @@
 package artofillusion.spmanager;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ManagersUtilsParserTest {
@@ -33,5 +34,15 @@ public class ManagersUtilsParserTest {
 
         System.out.println(d1);
         System.out.println(d2);
+    }
+
+    @Test
+    public void testParseVersion() {
+
+        Assert.assertEquals(1002, SPManagerUtils.parseVersion("1.2"));
+        Assert.assertEquals(1020, SPManagerUtils.parseVersion("1.20"));
+        Assert.assertEquals(1020003, SPManagerUtils.parseVersion("1.20.3"));
+
+        com.github.zafarkhaja.semver.Version version = new
     }
 }
