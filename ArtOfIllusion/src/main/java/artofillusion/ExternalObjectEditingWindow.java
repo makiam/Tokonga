d14977f351e9a9abb3c6f10099dc4cf305feae49
makiam <maxtemplaar@gmail.com>
2023-10-07 23:56:59 +0300

    Reduce BFC usages

--- ArtOfIllusion/src/main/java/artofillusion/ExternalObjectEditingWindow.java
+++ ArtOfIllusion/src/main/java/artofillusion/ExternalObjectEditingWindow.java
@@ -18,6 +18,7 @@ import buoy.event.*;
 
 import java.awt.*;
 import java.io.*;
+import javax.swing.JFileChooser;
 
 /**
  * This is a window for editing ExternalObjects.
@@ -87,13 +88,15 @@ public class ExternalObjectEditingWindow extends BDialog {
      * Allow the user to select a file.
      */
     private void doBrowseFile() {
-        BFileChooser fc = new BFileChooser(BFileChooser.OPEN_FILE, Translate.text("externalObject.selectScene"));
+        var chooser = new JFileChooser();
+        chooser.setName(Translate.text("externalObject.selectScene"));
+
         File f = theObject.getExternalSceneFile();
         if (f.isFile()) {
-            fc.setSelectedFile(f);
+            chooser.setSelectedFile(f);
         }
-        if (fc.showDialog(this)) {
-            fileField.setText(fc.getSelectedFile().getAbsolutePath());
+        if (chooser.showOpenDialog(this.getComponent()) == JFileChooser.APPROVE_OPTION) {
+            fileField.setText(chooser.getSelectedFile().getAbsolutePath());
             loadExternalScene();
             buildObjectTree();
             selectionChanged();
