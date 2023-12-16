5ed6de31bb02626ba47c99994a33d524c2f306bc
makiam <maxtemplaar@gmail.com>
2023-12-16 23:00:18 +0300

    Script editor update

--- ArtOfIllusion/src/main/java/artofillusion/script/ScriptEditorEx.java
+++ ArtOfIllusion/src/main/java/artofillusion/script/ScriptEditorEx.java
@@ -13,16 +13,19 @@ package artofillusion.script;
 import artofillusion.ArtOfIllusion;
 import artofillusion.LayoutWindow;
 import artofillusion.ui.Translate;
+import groovy.lang.GroovyRuntimeException;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
-import java.io.File;
 import java.io.IOException;
 
 import groovy.lang.GroovyShell;
+import groovy.lang.Script;
 import lombok.extern.slf4j.Slf4j;
+import org.codehaus.groovy.control.CompilationFailedException;
+import org.codehaus.groovy.runtime.StackTraceUtils;
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
 import org.fife.ui.rsyntaxtextarea.Theme;
@@ -35,12 +38,14 @@ public class ScriptEditorEx extends JFrame {
     private StatusPanel statusPanel;
     private JLabel statusLabel;
     private RSyntaxTextArea scriptTextArea;
+    private JTextArea outputArea;
     
     private org.fife.ui.rtextarea.RTextScrollPane scroller;
     
     private static final long serialVersionUID = 1L;
     private LayoutWindow layout;
     private JMenu editMenu;
+    private JCheckBoxMenuItem cleanOut;
     
     public ScriptEditorEx(LayoutWindow owner) {
         this.layout = owner;
@@ -57,7 +62,15 @@ public class ScriptEditorEx extends JFrame {
         scriptTextArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_GROOVY);
         scroller = new org.fife.ui.rtextarea.RTextScrollPane(scriptTextArea);
         scroller.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-        getContentPane().add(scroller, BorderLayout.CENTER);
+        JSplitPane esp = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+        esp.setTopComponent(scroller);
+        esp.setBottomComponent(outputArea = new JTextArea());
+       
+        outputArea.setFont(outputArea.getFont().deriveFont(12f));
+        outputArea.setLineWrap(true);
+        outputArea.setEditable(false);
+        outputArea.setWrapStyleWord(true);        
+        getContentPane().add(esp, BorderLayout.CENTER);
         
         editMenu.add(createMenuItem(RTextArea.getAction(RTextArea.UNDO_ACTION)));
         editMenu.add(createMenuItem(RTextArea.getAction(RTextArea.REDO_ACTION)));
@@ -98,6 +111,8 @@ public class ScriptEditorEx extends JFrame {
         var runMenu = mb.add(new JMenu(Translate.text("Run")));
         runMenu.add(new RunScriptAction());
         runMenu.add(new RunSelectedAction());
+        runMenu.addSeparator();
+        runMenu.add(cleanOut = new JCheckBoxMenuItem("Clean output on run"));
     }
 
     private static JMenuItem createMenuItem(Action action) {
@@ -114,9 +129,11 @@ public class ScriptEditorEx extends JFrame {
         private static final long serialVersionUID = 1L;
     }
 
+    
     private class NewScriptAction extends AbstractAction {
         public NewScriptAction() {
             super(Translate.text("New"));
+            putValue(AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
         }
 
         @Override
@@ -128,11 +145,12 @@ public class ScriptEditorEx extends JFrame {
     private class OpenScriptAction extends AbstractAction {
         public OpenScriptAction() {
             super(Translate.text("Open"));
+            putValue(AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_DOWN_MASK));
         }
 
         @Override
         public void actionPerformed(ActionEvent e) {
-
+            JFileChooser chooser = new JFileChooser();
         }
     }
 
@@ -146,29 +164,70 @@ public class ScriptEditorEx extends JFrame {
 
         }
     }
+    
 
+    private class ScriptPrinter {
+
+        public void print(String output) {
+            println(output);
+        }
+        
+        public void println(String output) {
+            ScriptEditorEx.this.outputArea.append(output + "\n");
+        }
+        
+        public void println() {
+            ScriptEditorEx.this.outputArea.append("\n");            
+        }
+        
+        public void print(Object output) {
+            println(output.toString());
+        }
+    }
     private class RunScriptAction extends AbstractAction {
         public RunScriptAction() {
             super(Translate.text("Run"));
             putValue(AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_F5, 0));
         }
         @Override
-        public void actionPerformed(ActionEvent e) {
-            var shell = ArtOfIllusion.getShell();
-            var script = shell.parse(ScriptEditorEx.this.scriptTextArea.getText());
-            script.setProperty("window", layout);
-            script.run();
+        public void actionPerformed(ActionEvent event) {
+            
+            Script script;
+            try {
+                script = shell.parse(ScriptEditorEx.this.scriptTextArea.getText());
+                log.atInfo().log("Script class: {}", script.getClass().getSimpleName());
+            } catch(CompilationFailedException cfe) {
+                ScriptEditorEx.this.outputArea.setText(cfe.getMessage());
+                return;
+            }
+            script.setProperty("out", new ScriptPrinter());
+            script.setProperty("window", layout);            
+            try {
+                script.run();
+            } catch (GroovyRuntimeException gre) {
+                log.atError().setCause(gre).log("Groovy script exception: {}", gre);
+                ScriptEditorEx.this.outputArea.append("\n" + gre.getMessage());
+            } catch(Exception e) {
+                
+                Throwable tt = StackTraceUtils.sanitize(e);
+                log.atError().setCause(e).log("Script exception: {}", tt);
+                ScriptEditorEx.this.outputArea.append("\n" + tt.getMessage());                
+                
+                
+            }
+
+            
             
         }
     }
-
+    
     private class RunSelectedAction extends AbstractAction {
         public RunSelectedAction() {
             super(Translate.text("Run Selection"));
         }
         @Override
         public void actionPerformed(ActionEvent e) {
-            var shell = ArtOfIllusion.getShell();
+            
         }
     }
     
