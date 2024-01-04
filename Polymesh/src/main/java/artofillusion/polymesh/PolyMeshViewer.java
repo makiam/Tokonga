10140dddd719ca134568eb854e2de0dae340c1f6
makiam <maxtemplaar@gmail.com>
2024-01-04 19:34:12 +0300

    Update drawVertices drawing code

--- Polymesh/src/main/java/artofillusion/polymesh/PolyMeshViewer.java
+++ Polymesh/src/main/java/artofillusion/polymesh/PolyMeshViewer.java
@@ -333,26 +333,15 @@ public class PolyMeshViewer extends MeshViewer {
                 ref = i;
             }
 
-            if (renderMode == RENDER_WIREFRAME || renderMode == RENDER_TRANSPARENT) {
-                if (!selected[ref] && visible[i]) {
-                    drawBox(screenVert[i].x - handleSize / 2, screenVert[i].y - handleSize / 2, handleSize, handleSize, vertColor);
-                }
+
+            var color = selected[ref] && visible[i] ? selectedVertColor : vertColor;
+
+            if(renderMode == RENDER_WIREFRAME || renderMode == RENDER_TRANSPARENT) {
+                drawBox(screenVert[i].x - handleSize / 2, screenVert[i].y - handleSize / 2, handleSize, handleSize, color);
             } else {
-                if (!selected[ref] && visible[i]) {
-                    renderBox(screenVert[i].x - handleSize / 2, screenVert[i].y - handleSize / 2, handleSize, handleSize, screenZ[i] - 0.01, vertColor);
-                }
+                renderBox(screenVert[i].x - handleSize / 2, screenVert[i].y - handleSize / 2, handleSize, handleSize, screenZ[i] - 0.01, color);
             }
 
-            // Now draw the selected portions.
-            if (renderMode == RENDER_WIREFRAME || renderMode == RENDER_TRANSPARENT) {
-                if (selected[ref] && visible[i]) {
-                    drawBox(screenVert[i].x - handleSize / 2, screenVert[i].y - handleSize / 2, handleSize, handleSize, selectedVertColor);
-                }
-            } else {
-                if (selected[ref] && visible[i]) {
-                    renderBox(screenVert[i].x - handleSize / 2, screenVert[i].y - handleSize / 2, handleSize, handleSize, screenZ[i] - 0.01, selectedVertColor);
-                }
-            }
         }
     }
 
