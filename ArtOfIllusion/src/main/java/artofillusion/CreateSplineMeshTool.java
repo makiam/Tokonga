84e4806eff4c5771149b69e644f039b5e86553ed
makiam <maxtemplaar@gmail.com>
2024-02-26 22:47:01 +0800

    Update CreateSplineMeshTool.java
    SonarLint warnings fix

--- ArtOfIllusion/src/main/java/artofillusion/CreateSplineMeshTool.java
+++ ArtOfIllusion/src/main/java/artofillusion/CreateSplineMeshTool.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 1999-2008 by Peter Eastman
    Changes copyright (C) 2019 by Petri Ihalainen
-   Changes copyright (C) 2020-2023 by Maksim Khramov
+   Changes copyright (C) 2020-2024 by Maksim Khramov
 
    This program is free software; you can redistribute it and/or modify it under the
    terms of the GNU General Public License as published by the Free Software
@@ -12,13 +12,14 @@
 
 package artofillusion;
 
-import artofillusion.animation.*;
 import artofillusion.math.*;
 import artofillusion.object.*;
 import artofillusion.ui.*;
 import buoy.event.*;
 import buoy.widget.*;
+
 import java.awt.*;
+import java.util.Arrays;
 
 /**
  * CreateSplineMeshTool is an EditingTool used for creating SplineMesh objects.
@@ -32,12 +33,17 @@ public class CreateSplineMeshTool extends EditingTool {
     static final int CYLINDER = 1;
     static final int TORUS = 2;
 
-    private boolean shiftDown, equilateral, centered;
+    private boolean equilateral;
+    private boolean centered;
     private Point clickPoint;
     private ObjectInfo info;
     private SplineMesh mesh;
-    private Vec3 ydir, zdir;
-    private int usize = 5, vsize = 5, shape = FLAT, smoothing = Mesh.APPROXIMATING;
+    private Vec3 yDir;
+    private Vec3 zDir;
+    private int uSize = 5;
+    private int vSize = 5;
+    private int shape = FLAT;
+    private int smoothing = Mesh.APPROXIMATING;
     private final int[] usizefor = new int[]{5, 8, 8};
     private final int[] vsizefor = new int[]{5, 5, 8};
     private double thickness = 0.5;
@@ -53,7 +59,8 @@ public class CreateSplineMeshTool extends EditingTool {
     }
 
     private void setHelpText() {
-        String shapeDesc, smoothingDesc;
+        String shapeDesc;
+        String smoothingDesc;
         if (shape == FLAT) {
             shapeDesc = "flat";
         } else if (shape == CYLINDER) {
@@ -66,9 +73,9 @@ public class CreateSplineMeshTool extends EditingTool {
         } else {
             smoothingDesc = "approximating";
         }
-        theWindow.setHelpText(Translate.text("createSplineMeshTool.helpText",
-                new Object[]{Integer.toString(usize), Integer.toString(vsize),
-                    Translate.text("createSplineMeshTool." + shapeDesc).toLowerCase(), Translate.text("menu." + smoothingDesc).toLowerCase()}));
+        String helpText = Translate.text("createSplineMeshTool.helpText", uSize, vSize,
+                Translate.text("createSplineMeshTool." + shapeDesc).toLowerCase(), Translate.text("menu." + smoothingDesc).toLowerCase());
+        theWindow.setHelpText(helpText);
     }
 
     @Override
@@ -76,17 +83,17 @@ public class CreateSplineMeshTool extends EditingTool {
         clickPoint = e.getPoint();
         equilateral = e.isShiftDown();
         centered = e.isControlDown();
-        ydir = Vec3.vy();
-        zdir = Vec3.vz();
+        yDir = Vec3.vy();
+        zDir = Vec3.vz();
     }
 
     @Override
     public void mouseDragged(WidgetMouseEvent e, ViewerCanvas view) {
         Camera cam = view.getCamera();
         Point dragPoint = e.getPoint();
-        ydir.set(cam.getCameraCoordinates().getUpDirection());
-        zdir.set(cam.getCameraCoordinates().getZDirection());
-        zdir.scale(-1.0);
+        yDir.set(cam.getCameraCoordinates().getUpDirection());
+        zDir.set(cam.getCameraCoordinates().getZDirection());
+        zDir.scale(-1.0);
 
         if (info == null) {
             // Create the initial mesh, if the mouse has moved enough. The limit is there to reduce
@@ -95,18 +102,15 @@ public class CreateSplineMeshTool extends EditingTool {
             if (Math.abs(dragPoint.x - clickPoint.x) + Math.abs(dragPoint.y - clickPoint.y) > 3) {
                 Scene theScene = ((LayoutWindow) theWindow).getScene();
                 Vec3[][] v = getMeshPoints(1.0, 1.0);
-                float[] usmoothness = new float[usize], vsmoothness = new float[vsize];
-                int i;
-                for (i = 0; i < usize; i++) {
-                    usmoothness[i] = 1.0f;
-                }
-                for (i = 0; i < vsize; i++) {
-                    vsmoothness[i] = 1.0f;
-                }
-                mesh = new SplineMesh(v, usmoothness, vsmoothness, smoothing, shape != FLAT, shape == TORUS);
+                float[] uSmoothness = new float[uSize];
+                float[] vSmoothness = new float[vSize];
+
+                Arrays.fill(uSmoothness, 1.0f);
+                Arrays.fill(vSmoothness, 1.0f);
+
+                mesh = new SplineMesh(v, uSmoothness, vSmoothness, smoothing, shape != FLAT, shape == TORUS);
                 info = new ObjectInfo(mesh, new CoordinateSystem(), "Spline Mesh " + (counter++));
-                info.addTrack(new PositionTrack(info), 0);
-                info.addTrack(new RotationTrack(info), 1);
+
                 UndoRecord undo = new UndoRecord(theWindow);
                 int[] sel = ((LayoutWindow) theWindow).getSelectedIndices();
                 ((LayoutWindow) theWindow).addObject(info, undo);
@@ -118,9 +122,15 @@ public class CreateSplineMeshTool extends EditingTool {
             }
         }
 
-        // Determine the size and position for the sphere.
-        Vec3 v1, v2, v3, orig;
-        double xsize, ysize, zsize;
+        // Determine the size and position for the SplineMesh.
+
+        Vec3 v1;
+        Vec3 v2;
+        Vec3 v3;
+        Vec3 orig;
+
+        double xSize;
+        double ySize;
 
         if (equilateral) {
             if (Math.abs(dragPoint.x - clickPoint.x) > Math.abs(dragPoint.y - clickPoint.y)) {
@@ -143,17 +153,17 @@ public class CreateSplineMeshTool extends EditingTool {
 
         if (centered) {
             orig = v1;
-            xsize = v2.minus(v1).length() * 2.0;
-            ysize = v2.minus(v3).length() * 2.0;
+            xSize = v2.minus(v1).length() * 2.0;
+            ySize = v2.minus(v3).length() * 2.0;
         } else {
             orig = v1.plus(v3).times(0.5);
-            xsize = v2.minus(v1).length();
-            ysize = v2.minus(v3).length();
+            xSize = v2.minus(v1).length();
+            ySize = v2.minus(v3).length();
         }
-        mesh.setVertexPositions(getMeshPoints(xsize, ysize));
+        mesh.setVertexPositions(getMeshPoints(xSize, ySize));
 
         info.getCoords().setOrigin(orig);
-        info.getCoords().setOrientation(zdir, ydir);
+        info.getCoords().setOrientation(zDir, yDir);
         info.clearCachedMeshes();
         theWindow.setModified();
 
@@ -164,42 +174,50 @@ public class CreateSplineMeshTool extends EditingTool {
     public void mouseReleased(WidgetMouseEvent e, ViewerCanvas view) {
         mesh = null;
         info = null;
-        System.gc();
     }
 
-    private Vec3[][] getMeshPoints(double xsize, double ysize) {
-        Vec3[][] v = new Vec3[usize][vsize];
-        int i, j;
+    private Vec3[][] getMeshPoints(double xSize, double ySize) {
+        Vec3[][] v = new Vec3[uSize][vSize];
+
 
         if (shape == FLAT) {
-            double xmin = -xsize * 0.5, ymin = -ysize * 0.5;
-            double uscale = 1.0 / (usize - 1), vscale = 1.0 / (vsize - 1);
+            double xMin = -xSize * 0.5;
+            double yMin = -ySize * 0.5;
+            double uScale = 1.0 / (uSize - 1);
+            double vScale = 1.0 / (vSize - 1);
 
-            for (i = 0; i < usize; i++) {
-                for (j = 0; j < vsize; j++) {
-                    v[i][j] = new Vec3(xsize * i * uscale + xmin, ysize * j * vscale + ymin, 0.0);
+            for (int i = 0; i < uSize; i++) {
+                for (int j = 0; j < vSize; j++) {
+                    v[i][j] = new Vec3(xSize * i * uScale + xMin, ySize * j * vScale + yMin, 0.0);
                 }
             }
         } else if (shape == CYLINDER) {
-            double rad = xsize * 0.5, ymin = -ysize * 0.5;
-            double uscale = 2.0 * Math.PI / usize, vscale = 1.0 / (vsize - 1);
+            double rad = xSize * 0.5;
+            double yMin = -ySize * 0.5;
+            double uScale = 2.0 * Math.PI / uSize;
+            double vScale = 1.0 / (vSize - 1);
 
-            for (i = 0; i < usize; i++) {
-                for (j = 0; j < vsize; j++) {
-                    v[i][j] = new Vec3(rad * Math.sin(uscale * i), ysize * j * vscale + ymin, rad * Math.cos(uscale * i));
+            for (int i = 0; i < uSize; i++) {
+                for (int j = 0; j < vSize; j++) {
+                    v[i][j] = new Vec3(rad * Math.sin(uScale * i), ySize * j * vScale + yMin, rad * Math.cos(uScale * i));
                 }
             }
         } else {
-            double rad = Math.min(xsize, ysize) * 0.25 * thickness;
-            double radx = xsize * 0.5 - rad, rady = ysize * 0.5 - rad;
-            double uscale = 2.0 * Math.PI / usize, vscale = 2.0 * Math.PI / vsize;
-            Vec3 vr = new Vec3(), vc = new Vec3();
+            double rad = Math.min(xSize, ySize) * 0.25 * thickness;
+            double radX = xSize * 0.5 - rad;
+            double radY = ySize * 0.5 - rad;
+            double uScale = 2.0 * Math.PI / uSize;
+            double vScale = 2.0 * Math.PI / vSize;
+            Vec3 vr = new Vec3();
+            Vec3 vc = new Vec3();
 
-            for (i = 0; i < usize; i++) {
-                vc.set(radx * Math.cos(uscale * i), rady * Math.sin(uscale * i), 0.0);
-                vr.set(rad * Math.cos(uscale * i), rad * Math.sin(uscale * i), 0.0);
-                for (j = 0; j < vsize; j++) {
-                    v[i][j] = new Vec3(vc.x + vr.x * Math.cos(vscale * j), vc.y + vr.y * Math.cos(vscale * j), rad * Math.sin(vscale * j));
+            for (int i = 0; i < uSize; i++) {
+                double uScaleI = uScale * i;
+                vc.set(radX * Math.cos(uScaleI), radY * Math.sin(uScaleI), 0.0);
+                vr.set(rad * Math.cos(uScaleI), rad * Math.sin(uScaleI), 0.0);
+                for (int j = 0; j < vSize; j++) {
+                    double vScaleJ = vScale * j;
+                    v[i][j] = new Vec3(vc.x + vr.x * Math.cos(vScaleJ), vc.y + vr.y * Math.cos(vScaleJ), rad * Math.sin(vScaleJ));
                 }
             }
         }
@@ -208,7 +226,6 @@ public class CreateSplineMeshTool extends EditingTool {
 
     @Override
     public void iconDoubleClicked() {
-        int i, minu, minv;
 
         final BComboBox shapeChoice = new BComboBox(new String[]{
             Translate.text("Flat"),
@@ -222,8 +239,8 @@ public class CreateSplineMeshTool extends EditingTool {
         } else {
             shapeChoice.setSelectedIndex(2);
         }
-        final ValueField usizeField = new ValueField((double) usizefor[shape], ValueField.POSITIVE + ValueField.INTEGER);
-        final ValueField vsizeField = new ValueField((double) vsizefor[shape], ValueField.POSITIVE + ValueField.INTEGER);
+        final ValueField uSizeField = new ValueField((double) usizefor[shape], ValueField.POSITIVE + ValueField.INTEGER);
+        final ValueField vSizeField = new ValueField((double) vsizefor[shape], ValueField.POSITIVE + ValueField.INTEGER);
         BComboBox smoothingChoice = new BComboBox(new String[]{
             Translate.text("Interpolating"),
             Translate.text("Approximating")
@@ -239,25 +256,25 @@ public class CreateSplineMeshTool extends EditingTool {
         shapeChoice.addEventLink(ValueChangedEvent.class, new Object() {
             void processEvent() {
                 thicknessSlider.setEnabled(shapeChoice.getSelectedIndex() == 2);
-                usizeField.setValue(usizefor[shapeChoice.getSelectedIndex()]);
-                vsizeField.setValue(vsizefor[shapeChoice.getSelectedIndex()]);
+                uSizeField.setValue(usizefor[shapeChoice.getSelectedIndex()]);
+                vSizeField.setValue(vsizefor[shapeChoice.getSelectedIndex()]);
             }
         });
-        usizeField.addEventLink(ValueChangedEvent.class, new Object() {
+        uSizeField.addEventLink(ValueChangedEvent.class, new Object() {
             void processEvent() {
-                usizefor[shapeChoice.getSelectedIndex()] = (int) usizeField.getValue();
+                usizefor[shapeChoice.getSelectedIndex()] = (int) uSizeField.getValue();
             }
         });
-        vsizeField.addEventLink(ValueChangedEvent.class, new Object() {
+        vSizeField.addEventLink(ValueChangedEvent.class, new Object() {
             void processEvent() {
-                vsizefor[shapeChoice.getSelectedIndex()] = (int) vsizeField.getValue();
+                vsizefor[shapeChoice.getSelectedIndex()] = (int) vSizeField.getValue();
             }
         });
 
         ComponentsDialog dlg = new ComponentsDialog(theFrame, Translate.text("selectMeshSizeShape"),
                 new Widget[]{shapeChoice,
-                    usizeField,
-                    vsizeField,
+                    uSizeField,
+                    vSizeField,
                     smoothingChoice,
                     thicknessSlider},
                 new String[]{Translate.text("Shape"),
@@ -269,26 +286,26 @@ public class CreateSplineMeshTool extends EditingTool {
             return;
         }
 
-        minu = shapeChoice.getSelectedIndex() == 0 ? 2 : 3;
-        minv = shapeChoice.getSelectedIndex() == 2 ? 3 : 2;
-        if (usizeField.getValue() < minu) {
-            MessageDialog.create().withOwner(theFrame.getComponent()).error(Translate.text("uSizeTooSmall", minu));
+        int minU = shapeChoice.getSelectedIndex() == 0 ? 2 : 3;
+        int minV = shapeChoice.getSelectedIndex() == 2 ? 3 : 2;
+        if (uSizeField.getValue() < minU) {
+            MessageDialog.create().withOwner(theFrame.getComponent()).error(Translate.text("uSizeTooSmall", minU));
             return;
         }
-        if (vsizeField.getValue() < minv) {
-            MessageDialog.create().withOwner(theFrame.getComponent()).error(Translate.text("vSizeTooSmall", minv));
+        if (vSizeField.getValue() < minV) {
+            MessageDialog.create().withOwner(theFrame.getComponent()).error(Translate.text("vSizeTooSmall", minV));
             return;
         }
-        usize = (int) usizeField.getValue();
-        vsize = (int) vsizeField.getValue();
-        i = smoothingChoice.getSelectedIndex();
-        if (i == 0) {
+        uSize = (int) uSizeField.getValue();
+        vSize = (int) vSizeField.getValue();
+
+        if (smoothingChoice.getSelectedIndex() == 0) {
             smoothing = Mesh.INTERPOLATING;
         } else {
             smoothing = Mesh.APPROXIMATING;
         }
-        i = shapeChoice.getSelectedIndex();
-        switch (i) {
+
+        switch (shapeChoice.getSelectedIndex()) {
             case 2:
                 shape = TORUS;
                 break;
