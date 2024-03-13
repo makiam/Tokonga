a30ac35f56193136a93c1e6ee8a450b2c9e6d677
makiam <maxtemplaar@gmail.com>
2024-03-13 22:49:24 +0800

    Update PoseTrackProvider

--- TrackProviders/src/main/java/artofillusion/tools/PoseTrackProvider.java
+++ TrackProviders/src/main/java/artofillusion/tools/PoseTrackProvider.java
@@ -20,9 +20,20 @@ public class PoseTrackProvider implements TrackProvider {
 
     @Override
     public void create(Collection<ObjectInfo> objects, UndoRecord undo) {
+        Map<Boolean, List<ObjectInfo>> pon = objects.stream().collect(Collectors.partitioningBy(PoseTrackProvider::isPosable));
+
+
+        pon.get(true).forEach(obj -> obj.addTrack(new PoseTrack(obj), 0));
+        pon.get(false).forEach(obj -> obj.addTrack(new PoseTrack(obj), 0));
 
     }
 
+    /**
+     * Check scene ObjectInfo posable.
+     *
+     * @param  info	object to check
+     * @return  true if the object is posable
+     */
     private static boolean isPosable(ObjectInfo info) {
         return info.getObject().getPosableObject() != null;
     }
