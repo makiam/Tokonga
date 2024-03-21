2776bc41c8945a5f33e48053057ffc2ee470f8a5
makiam <maxtemplaar@gmail.com>
2024-03-21 18:09:23 +0800

    WIP: More track providers to add
    Signed-off-by: makiam <maxtemplaar@gmail.com>

---
+++ TrackProviders/src/main/java/artofillusion/tools/ConstraintTrackProvider.java
@@ -0,0 +1,31 @@
+/* Copyright (C) 2024 by Maksim Khramov
+   This program is free software; you can redistribute it and/or modify it under the
+   terms of the GNU General Public License as published by the Free Software
+   Foundation; either version 2 of the License, or (at your option) any later version.
+   This program is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+   PARTICULAR PURPOSE.  See the GNU General Public License for more details. */
+
+package artofillusion.tools;
+
+import artofillusion.UndoRecord;
+import artofillusion.animation.ConstraintTrack;
+import artofillusion.object.ObjectInfo;
+import artofillusion.ui.Translate;
+
+public class ConstraintTrackProvider implements TrackProvider {
+    @Override
+    public String getCategory() {
+        return TrackProvider.super.getCategory();
+    }
+
+    @Override
+    public String getName() {
+        return Translate.text("menu.constraintTrack");
+    }
+
+    @Override
+    public void forEach(ObjectInfo item, UndoRecord undo) {
+        TrackProvider.add(item, new ConstraintTrack(item), undo);
+    }
+}
---
+++ TrackProviders/src/main/java/artofillusion/tools/TwistDistortionTrackProvider.java
@@ -0,0 +1,31 @@
+/* Copyright (C) 2024 by Maksim Khramov
+   This program is free software; you can redistribute it and/or modify it under the
+   terms of the GNU General Public License as published by the Free Software
+   Foundation; either version 2 of the License, or (at your option) any later version.
+   This program is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+   PARTICULAR PURPOSE.  See the GNU General Public License for more details. */
+
+package artofillusion.tools;
+
+import artofillusion.UndoRecord;
+import artofillusion.animation.distortion.TwistTrack;
+import artofillusion.object.ObjectInfo;
+import artofillusion.ui.Translate;
+
+public class TwistDistortionTrackProvider implements TrackProvider {
+    @Override
+    public String getCategory() {
+        return Translate.text("menu.distortionTrack");
+    }
+
+    @Override
+    public String getName() {
+        return Translate.text("menu.twistDistortion");
+    }
+
+    @Override
+    public void forEach(ObjectInfo item, UndoRecord undo) {
+        TrackProvider.add(item, new TwistTrack(item), undo);
+    }
+}
