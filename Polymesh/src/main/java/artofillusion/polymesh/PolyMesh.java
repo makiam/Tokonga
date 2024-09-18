b5a64fefe3e3056a538ce53398840fcb92671e85
makiam <maxtemplaar@gmail.com>
2024-09-18 17:56:20 +0800

    Format planarMesh function code

--- Polymesh/src/main/java/artofillusion/polymesh/PolyMesh.java
+++ Polymesh/src/main/java/artofillusion/polymesh/PolyMesh.java
@@ -1037,9 +1037,7 @@ public final class PolyMesh extends Object3D implements FacetedMesh {
         faces = new Wface[euSize * evSize];
         for (int i = 0; i < uSize; ++i) {
             for (int j = 0; j < vSize; ++j) {
-                vertices[i + uSize * j] = new Wvertex(new Vec3(i * uMax
-                        / ((double) (uSize - 1)), j * vMax
-                        / ((double) (vSize - 1)), 0), -1);
+                vertices[i + uSize * j] = new Wvertex(new Vec3(i * uMax / ((double) (uSize - 1)), j * vMax / ((double) (vSize - 1)), 0), -1);
             }
         }
         int ec;
