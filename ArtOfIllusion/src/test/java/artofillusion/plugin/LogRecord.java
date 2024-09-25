2d9952878923f9bc826b73451f2d081500c8d48e
makiam <maxtemplaar@gmail.com>
2024-09-25 16:07:38 +0800

    WIP: Plugin API. Update log record

--- ArtOfIllusion/src/test/java/artofillusion/plugin/LogRecord.java
+++ ArtOfIllusion/src/test/java/artofillusion/plugin/LogRecord.java
@@ -2,13 +2,17 @@ package artofillusion.plugin;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
+import com.thoughtworks.xstream.annotations.XStreamConverter;
+import com.thoughtworks.xstream.converters.extended.ToAttributedValueConverter;
 import lombok.Data;
 
 
 @XStreamAlias("log")
 @Data
+@XStreamConverter(value = ToAttributedValueConverter.class, strings = {"text"})
 public class LogRecord {
     @XStreamAsAttribute private String version;
     @XStreamAsAttribute private String date;
     @XStreamAsAttribute private String author;
+    private String text;
 }
