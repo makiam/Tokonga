2df93074346693e9af7aa702e8771405295b1c2c
makiam <maxtemplaar@gmail.com>
2024-10-05 18:33:10 +0300

    WIP:Procedural Node XML data model

--- ArtOfIllusion/src/main/java/artofillusion/procedural/IOPort.java
+++ ArtOfIllusion/src/main/java/artofillusion/procedural/IOPort.java
@@ -12,6 +12,8 @@
 package artofillusion.procedural;
 
 import java.awt.*;
+import java.util.Arrays;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -118,4 +120,12 @@ public class IOPort {
         description = desc;
     }
 
+    @Override
+    public String toString() {
+        return "IOPort{" +
+                "valueType=" + (valueType  == 0 ? "number" : "color") +
+                ", type=" + (type == 0 ? "input" : "output") +
+                ", description=" + Arrays.toString(description) +
+                '}';
+    }
 }
---
+++ StandardModules/src/main/kotlin/artofillusion/procedural/RGBToHSVModule.kt
@@ -0,0 +1,51 @@
+package artofillusion.procedural
+
+import java.awt.Point
+
+@ProceduralModule.Category(value = "Modules:menu.colorFunctions")
+class RGBToHSVModule @JvmOverloads constructor(position: Point? = Point()) : ProceduralModule<RGBToHSVModule?>(
+    "RGB->HSV",
+    arrayOf<IOPort>(
+        IOPort(IOPort.NUMBER, IOPort.INPUT, IOPort.LEFT, *arrayOf<String>("Red", "(0)")),
+        IOPort(IOPort.NUMBER, IOPort.INPUT, IOPort.LEFT, *arrayOf<String>("Green", "(0)")),
+        IOPort(IOPort.NUMBER, IOPort.INPUT, IOPort.LEFT, *arrayOf<String>("Blue", "(0)"))
+    ),
+    arrayOf<IOPort>(
+        IOPort(IOPort.NUMBER, IOPort.OUTPUT, IOPort.RIGHT, *arrayOf<String>("Hue")),
+        IOPort(IOPort.NUMBER, IOPort.OUTPUT, IOPort.RIGHT, *arrayOf<String>("Saturation")),
+        IOPort(IOPort.NUMBER, IOPort.OUTPUT, IOPort.RIGHT, *arrayOf<String>("Value"))
+    ),
+    position) {
+    override fun getAverageValue(which: Int, blur: Double): Double {
+
+        val red: Double = this.linkFrom[0]?.getAverageValue(this.linkFromIndex[0], blur) ?: 0.0
+        val green: Double = this.linkFrom[1]?.getAverageValue(this.linkFromIndex[1], blur) ?: 0.0
+        val blue: Double  = this.linkFrom[2]?.getAverageValue(this.linkFromIndex[2], blur) ?: 0.0
+
+        return rgbToHsv(red, green, blue, which)
+    }
+
+    fun rgbToHsv(red: Double, green: Double, blue: Double, index: Int): Double {
+        val min = minOf(red, green, blue)
+        val max = maxOf(red, green, blue)
+        val delta = max - min
+
+        val hue = when {
+            delta == 0.0 -> 0.0
+            red == max -> 60.0 * (green - blue) / delta
+            green == max -> 60.0 * (blue - red) / delta + 120.0
+            else -> 60.0 * (red - green) / delta + 240.0
+        }
+
+        val saturation = if (max == 0.0) 0.0 else delta / max
+        val value = max
+
+        return when (index) {
+            0 -> hue
+            1 -> saturation
+            2 -> value
+            else -> 0.0
+        }
+    }
+
+}
