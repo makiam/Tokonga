d2b759c174b6caf8e9f435a54c2cee38f3ac684e
makiam <maxtemplaar@gmail.com>
2024-12-10 23:03:10 +0300

    Experimental Procedure Editor

--- ArtOfIllusion/src/main/java/artofillusion/material/ExperimentalProcedureEditor.java
+++ ArtOfIllusion/src/main/java/artofillusion/material/ExperimentalProcedureEditor.java
@@ -1,9 +1,29 @@
 package artofillusion.material;
 
+import artofillusion.ArtOfIllusion;
 import artofillusion.Scene;
 import artofillusion.procedural.Procedure;
+import buoy.widget.BFrame;
 
-public class ExperimentalProcedureEditor {
-    public ExperimentalProcedureEditor(Procedure proc, ProceduralMaterial3D proceduralMaterial3D, Scene sc) {
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ExperimentalProcedureEditor extends BFrame {
+
+    public ExperimentalProcedureEditor(Procedure proc, ProceduralMaterial3D owner, Scene scene) {
+        super();
+        this.getComponent().setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        this.getComponent().setTitle(owner.getWindowTitle());
+        this.getComponent().setIconImage(ArtOfIllusion.APP_ICON.getImage());
+        this.getComponent().getContentPane().setLayout(new BorderLayout());
+        this.getComponent().getContentPane().add(new JSplitPane(), java.awt.BorderLayout.CENTER);
+        this.setVisible(true);
+
+    }
+
+    @Override
+    protected JFrame createComponent() {
+        return new ExperimentalProcedureEditorWindow();
     }
 }
