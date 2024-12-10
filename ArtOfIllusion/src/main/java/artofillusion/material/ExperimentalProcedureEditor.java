bd02855e348c25a19fe8257197f5e910a2895021
makiam <maxtemplaar@gmail.com>
2024-12-10 20:54:55 +0300

    Experimental Procedure Editor selector

---
+++ ArtOfIllusion/src/main/java/artofillusion/material/ExperimentalProcedureEditor.java
@@ -0,0 +1,9 @@
+package artofillusion.material;
+
+import artofillusion.Scene;
+import artofillusion.procedural.Procedure;
+
+public class ExperimentalProcedureEditor {
+    public ExperimentalProcedureEditor(Procedure proc, ProceduralMaterial3D proceduralMaterial3D, Scene sc) {
+    }
+}
