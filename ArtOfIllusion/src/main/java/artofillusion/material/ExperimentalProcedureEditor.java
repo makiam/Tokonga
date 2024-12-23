2f4d128bdcf5d2bb5168c22ff1a0cddf481307a3
makiam <maxtemplaar@gmail.com>
2024-12-23 23:44:01 +0300

    WIP: New procedure editor

--- ArtOfIllusion/src/main/java/artofillusion/material/ExperimentalProcedureEditor.java
+++ ArtOfIllusion/src/main/java/artofillusion/material/ExperimentalProcedureEditor.java
@@ -1,27 +1,50 @@
+/* Copyright (C) 2024 by Maksim Khramov
+
+   This program is free software; you can redistribute it and/or modify it under the
+   terms of the GNU General Public License as published by the Free Software
+   Foundation; either version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+   PARTICULAR PURPOSE.  See the GNU General Public License for more details. */
+
 package artofillusion.material;
 
-import artofillusion.ArtOfIllusion;
 import artofillusion.Scene;
 import artofillusion.procedural.Procedure;
+import artofillusion.procedural.ProcedureOwner;
 import buoy.widget.BFrame;
 
 
 import javax.swing.*;
-import java.awt.*;
 
-public class ExperimentalProcedureEditor extends BFrame {
+import lombok.extern.slf4j.Slf4j;
 
-    public ExperimentalProcedureEditor(Procedure proc, ProceduralMaterial3D owner, Scene scene) {
+
+@Slf4j
+public final class ExperimentalProcedureEditor extends BFrame {
+    
+
+    private Procedure procedure;
+    private ProcedureOwner owner;
+    private Scene scene;
+    
+    public ExperimentalProcedureEditor(Procedure proc, ProcedureOwner owner, Scene scene) {
         super();
-        this.getComponent().setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        this.getComponent().setTitle(owner.getWindowTitle());
-        this.getComponent().setIconImage(ArtOfIllusion.APP_ICON.getImage());
-        this.getComponent().getContentPane().setLayout(new BorderLayout());
-        this.getComponent().getContentPane().add(new JSplitPane(), java.awt.BorderLayout.CENTER);
-        this.setVisible(true);
-
+        this.procedure = proc;
+        this.owner = owner;
+        this.scene = scene;
+        initPeer();
     }
 
+    private void initPeer() {
+        var peer = (ExperimentalProcedureEditorWindow)this.getComponent();
+        peer.setScene(scene);
+        peer.setProcedure(procedure);
+        peer.setProcedureOwner(owner);
+        peer.initPeer();
+        this.setVisible(true);        
+    }
     @Override
     protected JFrame createComponent() {
         return new ExperimentalProcedureEditorWindow();
