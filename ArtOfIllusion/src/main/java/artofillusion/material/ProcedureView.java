4241d3b9eda8b5a1d6cbbe5202f922982381de24
makiam <maxtemplaar@gmail.com>
2024-12-25 18:31:34 +0800

    Update procedure editor
    Update procedure editor

--- ArtOfIllusion/src/main/java/artofillusion/material/ProcedureView.java
+++ ArtOfIllusion/src/main/java/artofillusion/material/ProcedureView.java
@@ -13,7 +13,7 @@ package artofillusion.material;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
+import lombok.Getter;
 /**
  *
  * @author MaksK
@@ -23,6 +23,9 @@ class ProcedureView extends JComponent implements MouseListener, MouseWheelListe
     private static final Color NETWORK_BACKGROUND_COLOR = new Color(85, 85, 85);
     private static final int GRID_CELL_SIZE = 48;
     private static final int GRID_OFFSET = 6;
+
+    @Getter
+    private double viewX, viewY, viewScale = 1;
 
     public ProcedureView() {
         this.addMouseListener(this);
@@ -60,7 +63,34 @@ class ProcedureView extends JComponent implements MouseListener, MouseWheelListe
     }
 
     @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
+    protected void paintComponent(Graphics graphics) {
+        Graphics2D g2 = (Graphics2D) graphics;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+
+        // Draw background
+        g2.setColor(ProcedureView.NETWORK_BACKGROUND_COLOR);
+        g2.fill(graphics.getClipBounds());
+
+        // Draw grid
+        paintGrid(g2);
+    }
+
+    private void paintGrid(Graphics2D g) {
+        g.setColor(ProcedureView.NETWORK_GRID_COLOR);
+
+        int gridCellSize = (int) Math.round(GRID_CELL_SIZE * getViewScale());
+        int gridOffset = (int) Math.round(GRID_OFFSET * getViewScale());
+        if (gridCellSize < 10) return;
+
+        int transformOffsetX = (int) (getViewX() % gridCellSize);
+        int transformOffsetY = (int) (getViewY() % gridCellSize);
+
+        for (int y = -gridCellSize; y < getHeight() + gridCellSize; y += gridCellSize) {
+            g.drawLine(0, y - gridOffset + transformOffsetY, getWidth(), y - gridOffset + transformOffsetY);
+        }
+        for (int x = -gridCellSize; x < getWidth() + gridCellSize; x += gridCellSize) {
+            g.drawLine(x - gridOffset + transformOffsetX, 0, x - gridOffset + transformOffsetX, getHeight());
+        }
     }
 }
