61c18574b58e08adb05a5eeefb55d9ef36af7a7a
makiam <maxtemplaar@gmail.com>
2025-04-07 20:48:20 +0800

    ModuleSupplier

--- ArtOfIllusion/src/test/java/artofillusion/procedural/ModuleSupplerTest.java
+++ ArtOfIllusion/src/test/java/artofillusion/procedural/ModuleSupplerTest.java
@@ -1,12 +1,15 @@
 package artofillusion.procedural;
 
+import artofillusion.test.util.RegisterTestResources;
+import artofillusion.test.util.SetupLocale;
+import artofillusion.test.util.SetupLookAndFeel;
+import artofillusion.test.util.SetupTheme;
+import artofillusion.ui.Translate;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.util.function.Consumer;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith({SetupLocale.class})
 class ModuleSupplerTest {
 
     @Test
@@ -14,10 +17,33 @@ class ModuleSupplerTest {
         CoordinateModule cm = new CoordinateModule();
 
         ModuleSuppler<CoordinateModule> cms = new ModuleSuppler<>(CoordinateModule.class, it ->
-            it.setCoordinate(CoordinateModule.X));
+            it.setCoordinate(CoordinateModule.Y));
         var neww = cms.get();
         Assertions.assertNotEquals(cm, neww);
-        Assertions.assertEquals(neww.coordinate, CoordinateModule.X);
+        Assertions.assertEquals(CoordinateModule.Y, neww.coordinate);
+        Assertions.assertEquals("Y", neww.getName());
     }
 
+    @Test
+    public void testDefaultModuleSupplier() {
+        CoordinateModule cm = new CoordinateModule();
+
+        ModuleSuppler<CoordinateModule> cms = new ModuleSuppler<>(CoordinateModule.class);
+        var neww = cms.get();
+        Assertions.assertNotEquals(cm, neww);
+        Assertions.assertEquals(CoordinateModule.X, neww.coordinate);
+        Assertions.assertEquals("X", neww.getName());
+    }
+
+    @Test
+    public void testTimeCoordinateModuleSupplier() {
+        CoordinateModule cm = new CoordinateModule();
+
+        ModuleSuppler<CoordinateModule> cms = new ModuleSuppler<>(CoordinateModule.class, it ->
+                it.setCoordinate(CoordinateModule.T));
+        var neww = cms.get();
+        Assertions.assertNotEquals(cm, neww);
+        Assertions.assertEquals(CoordinateModule.T, neww.coordinate);
+        Assertions.assertEquals(Translate.text("Time"), neww.getName());
+    }
 }
