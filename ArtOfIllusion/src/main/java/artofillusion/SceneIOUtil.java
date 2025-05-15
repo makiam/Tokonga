901cf164363156886cb6b999312810b59737df08
makiam <maxtemplaar@gmail.com>
2025-05-15 21:25:32 +0800

    WIP: Extract Scene IO code

--- ArtOfIllusion/src/main/java/artofillusion/SceneIOUtil.java
+++ ArtOfIllusion/src/main/java/artofillusion/SceneIOUtil.java
@@ -1,5 +1,6 @@
 package artofillusion;
 
+import artofillusion.animation.Track;
 import artofillusion.image.ImageMap;
 import artofillusion.image.MIPMappedImage;
 import artofillusion.object.ObjectInfo;
@@ -26,20 +27,21 @@ final class SceneIOUtil {
     }
 
     public static void loadImageMapsUnbuffered(DataInputStream in, Scene scene) throws IOException {
-        var images = in.readInt();
-        log.debug("Read images: {}", images);
+        var counter = in.readInt();
+        log.debug("Read images: {}", counter);
 
-        for (int i = 0; i < images; i++) {
+        for (int i = 0; i < counter; i++) {
             String className = in.readUTF();
             try {
                 Class<?> cls = ArtOfIllusion.getClass(className);
                 if (cls == null) {
                     throw new IOException("Unknown ImageMap class: " + className);
                 }
-                Constructor<?> con = cls.getConstructor(DataInputStream.class);
-                scene.add((ImageMap) con.newInstance(in));
-            } catch (IOException | ReflectiveOperationException | SecurityException ex) {
-                throw new IOException("Error loading image: " + ex.getMessage());
+                scene.add((ImageMap) cls.getConstructor(DataInputStream.class).newInstance(in));
+            } catch (IOException | ReflectiveOperationException rex) {
+                throw new IOException("Error loading image: " + rex.getMessage());
+            } catch(SecurityException se) {
+                throw new IOException("Error loading image: " + se.getMessage());
             }
         }
 
@@ -59,6 +61,23 @@ final class SceneIOUtil {
     public static void loadTracksUnbuffered(DataInputStream in, Scene scene, ObjectInfo owner) throws IOException {
         var tracks = in.readInt();
         log.debug("Read tracks: {}", tracks);
+
+        for (int i = 0; i < tracks; i++) {
+            String className = in.readUTF();
+            try {
+                Class<?> cls = ArtOfIllusion.getClass(className);
+                if (cls == null) {
+                    throw new IOException("Unknown Track class: " + className);
+                }
+                var tr = (Track<?>) cls.getConstructor(ObjectInfo.class).newInstance(owner);
+                tr.initFromStream(in, scene);
+                owner.addTrack(tr);
+            } catch (IOException | ReflectiveOperationException rex) {
+                throw new IOException("Error loading Track: " + rex.getMessage());
+            } catch(SecurityException se) {
+                throw new IOException("Error loading Track: " + se.getMessage());
+            }
+        }
     }
 
 }
