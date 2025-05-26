03f2937eab42ac698a20ce8de1b79177308c15b6
makiam <maxtemplaar@gmail.com>
2025-05-26 21:53:27 +0300

    Add SceneIO

---
+++ ArtOfIllusion/src/main/java/artofillusion/SceneIO.java
@@ -0,0 +1,23 @@
+/* Copyright 2025 by Maksim Khramov
+
+   This program is free software; you can redistribute it and/or modify it under the
+   terms of the GNU General Public License as published by the Free Software
+   Foundation; either version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+   PARTICULAR PURPOSE.  See the GNU General Public License for more details. */
+
+
+package artofillusion;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.greenrobot.eventbus.EventBus;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Slf4j
+public class SceneIO {
+    private static EventBus bus = org.greenrobot.eventbus.EventBus.getDefault();
+}
