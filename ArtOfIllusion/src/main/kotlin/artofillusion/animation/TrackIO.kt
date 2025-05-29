0bd9d95235acc458d0e131cc44a95aabe869d7b6
makiam <maxtemplaar@gmail.com>
2025-05-29 21:06:46 +0800

    WIP: SceneIO for Tracks

--- ArtOfIllusion/src/main/kotlin/artofillusion/animation/TrackIO.kt
+++ ArtOfIllusion/src/main/kotlin/artofillusion/animation/TrackIO.kt
@@ -6,12 +6,16 @@ import org.greenrobot.eventbus.EventBus
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.DataInputStream
+import java.io.DataOutput
+import java.io.DataOutputStream
+import java.io.IOException
 
 class TrackIO private constructor() {
     private val bus: EventBus = EventBus.getDefault()
     private val log: Logger = LoggerFactory.getLogger(TrackIO::class.java)
 
-    fun readTracks(input: DataInputStream, scene: Scene, owner: ObjectInfo, version: Int): Unit {
+    @Throws(IOException::class)
+    fun readTracks(input: DataInputStream, scene: Scene, owner: ObjectInfo, version: Int) {
         val tracks = input.readInt()
         log.debug("Scene version {}. Reading {} tracks for {}.", version, tracks, owner.name )
 
@@ -22,13 +26,34 @@ class TrackIO private constructor() {
         log.debug("Read tracks for {} completed", owner.name)
     }
 
-    fun readTracksV6(input: DataInputStream, scene: Scene, owner: ObjectInfo, tracks: Int): Unit {
+    @Throws(IOException::class)
+    fun writeTracks(output: DataOutputStream, scene: Scene, owner: ObjectInfo, version: Int) {
 
     }
 
-    fun readTracksV5(input: DataInputStream, scene: Scene, owner: ObjectInfo, tracks: Int): Unit {
+    @Throws(IOException::class)
+    fun writeTrack(output: DataOutputStream, scene: Scene, owner: ObjectInfo, track: Track<*>, version: Int) {
+        log.debug("Write track: {} of {} for {}. Version {}", track.name, track.javaClass.name, owner.name, version)
+            writeClass(output, track)
+            track.writeToStream(output, scene)
+        log.debug("Write track completed")
+    }
+
+    @Throws(IOException::class)
+    fun readTracksV6(input: DataInputStream, scene: Scene, owner: ObjectInfo, tracks: Int) {
 
     }
 
+    @Throws(IOException::class)
+    fun readTracksV5(input: DataInputStream, scene: Scene, owner: ObjectInfo, tracks: Int) {
+
+    }
+
+    @Throws(IOException::class)
     fun readString(input: DataInputStream): String = input.readUTF()
+
+    @Throws(IOException::class)
+    fun writeClass(output: DataOutputStream, item: Any) {
+        output.writeUTF(item.javaClass.name)
+    }
 }
