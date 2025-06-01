88ee29f853158191bf5e240f7cb6f1a45eb98f01
makiam <maxtemplaar@gmail.com>
2025-06-01 11:04:45 +0300

    Update test

--- ArtOfIllusion/src/test/java/artofillusion/animation/TrackRestoreTest.java
+++ ArtOfIllusion/src/test/java/artofillusion/animation/TrackRestoreTest.java
@@ -16,8 +16,10 @@ import artofillusion.object.Cube;
 import artofillusion.object.Object3D;
 import artofillusion.object.ObjectInfo;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
@@ -53,6 +55,37 @@ public class TrackRestoreTest {
 
     }
 
+    @Test
+    void testWriteAndRestoreSceneItemTracksV6() throws IOException {
+        short version = 6;
+
+        Scene scene = new Scene();
+        Object3D obj = new Cube(1, 1, 1);
+        ObjectInfo owner = new ObjectInfo(obj, new CoordinateSystem(), "Cube");
+        PoseTrack track = new PoseTrack(owner);
+        track.setName("My Pose");
+        track.setSmoothingMethod(Timecourse.APPROXIMATING);
+
+        owner.addTrack(track);
+        owner.addTrack(track);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream(1000);
+        DataOutputStream data = new DataOutputStream(out);
+
+        TrackIO.INSTANCE.writeTracks(data, scene, owner, version);
+        owner.removeTrack(0);
+        owner.removeTrack(0);
+
+        byte[] buffer = out.toByteArray();
+        ByteArrayInputStream in = new ByteArrayInputStream(buffer);
+        TrackIO.INSTANCE.readTracks(new DataInputStream(in), scene, owner, version);
+
+        Assertions.assertEquals(2, owner.getTracks().length);
+        PoseTrack restored = (PoseTrack)owner.getTracks()[0];
+        Assertions.assertEquals("My Pose", restored.getName());
+        Assertions.assertEquals(Timecourse.APPROXIMATING, restored.getSmoothingMethod());
+    }
+
 
     @Test
     void testWriteAndRestoreSceneItemTrack() throws IOException {
@@ -83,11 +116,12 @@ public class TrackRestoreTest {
     }
 
     @Test
-    void testTryToRestoreBadTrackOldVersion() throws IOException{
+    @DisplayName("Error to Restore track as no appropriate constructor")
+    void testTryToRestoreBadTrackOldVersion() throws IOException {
         Scene scene = new Scene();
         Object3D obj = new Cube(1, 1, 1);
-        ObjectInfo owner = new ObjectInfo(obj, new CoordinateSystem(), "Cube");
-        PoseTrack track = new PrivatePoseTrack(owner);
+        ObjectInfo owner = new ObjectInfo(obj, new CoordinateSystem(), "Likörflasche PM");
+        PoseTrack track = new LocalPoseTrack(owner);
         track.setName("My Pose");
         track.setSmoothingMethod(Timecourse.APPROXIMATING);
 
@@ -104,9 +138,52 @@ public class TrackRestoreTest {
 
     }
 
-    private class PrivatePoseTrack extends PoseTrack {
+    @Test
+    void testRestoreStringContainer() throws IOException {
+        Scene scene = new Scene();
+        Object3D obj = new Cube(1, 1, 1);
+        ObjectInfo owner = new ObjectInfo(obj, new CoordinateSystem(), "Cube");
 
-        public PrivatePoseTrack(ObjectInfo info) {
+        Track track = new StringContainerTrack(owner);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream(1000);
+        DataOutputStream data = new DataOutputStream(out);
+        data.writeInt(1);
+        TrackIO.INSTANCE.writeTrack(data, scene, track, (short)5);
+
+        byte[] buffer = out.toByteArray();
+        ByteArrayInputStream in = new ByteArrayInputStream(buffer);
+        TrackIO.INSTANCE.readTracks(new DataInputStream(in), scene, owner, (short)5);
+
+        Assertions.assertEquals(1, owner.getTracks().length);
+        StringContainerTrack restored = (StringContainerTrack)owner.getTracks()[0];
+
+        Assertions.assertEquals("А роза упала на лапу азора", restored.getPalindrome());
+    }
+
+    static class StringContainerTrack extends DummyTrack {
+
+        @Getter
+        private String palindrome = "";
+
+        public StringContainerTrack(ObjectInfo info) {
+
+        }
+
+        @Override
+        public void writeToStream(DataOutputStream out, Scene scene) throws IOException {
+            out.writeUTF("А роза упала на лапу азора");
+        }
+
+        @Override
+        public void initFromStream(DataInputStream in, Scene scene) throws IOException {
+            palindrome = in.readUTF();
+        }
+    }
+
+    private class LocalPoseTrack extends PoseTrack {
+
+        public LocalPoseTrack(ObjectInfo info) {
             super(info);
         }
     }
