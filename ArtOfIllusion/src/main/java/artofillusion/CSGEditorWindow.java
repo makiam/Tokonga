bd00c2051c398f449cf343273b6f9046ccc247e1
Maksim Khramov <khramov.wx1235557@huawei-partners.com>
2026-02-20 14:21:39 +0300

    Use extended for-loop

--- ArtOfIllusion/src/main/java/artofillusion/CSGEditorWindow.java
+++ ArtOfIllusion/src/main/java/artofillusion/CSGEditorWindow.java
@@ -72,9 +72,9 @@ public class CSGEditorWindow extends ObjectEditorWindow {
         tools.addTool(metaTool = new MoveViewTool(this));
         tools.addTool(altTool = new RotateViewTool(this));
         tools.selectTool(defaultTool);
-        for (int i = 0; i < theView.length; i++) {
-            theView[i].setMetaTool(metaTool);
-            theView[i].setAltTool(altTool);
+        for(ViewerCanvas viewerCanvas: theView) {
+            viewerCanvas.setMetaTool(metaTool);
+            viewerCanvas.setAltTool(altTool);
         }
         UIUtilities.applyDefaultFont(content);
         UIUtilities.applyDefaultBackground(content);
@@ -375,39 +375,39 @@ public class CSGEditorWindow extends ObjectEditorWindow {
         values = dlg.getValues();
         UndoRecord undo = new UndoRecord(this);
         setUndoRecord(undo);
-        for (int i = 0; i < sel.length; i++) {
-            obj = theScene.getObject(sel[i]).getObject();
-            coords = theScene.getObject(sel[i]).getCoords();
+        for(int j: sel) {
+            obj = theScene.getObject(j).getObject();
+            coords = theScene.getObject(j).getCoords();
             undo.addCommand(UndoRecord.COPY_OBJECT, obj, obj.duplicate());
             undo.addCommand(UndoRecord.COPY_COORDS, coords, coords.duplicate());
             orig = coords.getOrigin();
             size = obj.getBounds().getSize();
-            if (!Double.isNaN(values[0])) {
+            if(!Double.isNaN(values[0])) {
                 orig.x += values[0];
             }
-            if (!Double.isNaN(values[1])) {
+            if(!Double.isNaN(values[1])) {
                 orig.y += values[1];
             }
-            if (!Double.isNaN(values[2])) {
+            if(!Double.isNaN(values[2])) {
                 orig.z += values[2];
             }
             m = Mat4.identity();
-            if (!Double.isNaN(values[3])) {
+            if(!Double.isNaN(values[3])) {
                 m = m.times(Mat4.xrotation(values[3] * Math.PI / 180.0));
             }
-            if (!Double.isNaN(values[4])) {
+            if(!Double.isNaN(values[4])) {
                 m = m.times(Mat4.yrotation(values[4] * Math.PI / 180.0));
             }
-            if (!Double.isNaN(values[5])) {
+            if(!Double.isNaN(values[5])) {
                 m = m.times(Mat4.zrotation(values[5] * Math.PI / 180.0));
             }
-            if (!Double.isNaN(values[6])) {
+            if(!Double.isNaN(values[6])) {
                 size.x *= values[6];
             }
-            if (!Double.isNaN(values[7])) {
+            if(!Double.isNaN(values[7])) {
                 size.y *= values[7];
             }
-            if (!Double.isNaN(values[8])) {
+            if(!Double.isNaN(values[8])) {
                 size.z *= values[8];
             }
             coords.setOrigin(orig);
@@ -477,83 +477,83 @@ public class CSGEditorWindow extends ObjectEditorWindow {
 
         // Determine the position to align the objects to.
         alignTo = new Vec3();
-        for (int i = 0; i < sel.length; i++) {
-            obj = theScene.getObject(sel[i]).getObject();
-            coords = theScene.getObject(sel[i]).getCoords();
+        for(int j: sel) {
+            obj = theScene.getObject(j).getObject();
+            coords = theScene.getObject(j).getCoords();
             bounds = obj.getBounds();
             bounds = bounds.transformAndOutset(coords.fromLocal());
             center = bounds.getCenter();
             orig = coords.getOrigin();
-            if (!Double.isNaN(vfx.getValue())) {
+            if(!Double.isNaN(vfx.getValue())) {
                 alignTo.x += vfx.getValue();
-            } else if (xchoice.getSelectedIndex() == 1) {
+            } else if(xchoice.getSelectedIndex() == 1) {
                 alignTo.x += bounds.maxx;
-            } else if (xchoice.getSelectedIndex() == 2) {
+            } else if(xchoice.getSelectedIndex() == 2) {
                 alignTo.x += center.x;
-            } else if (xchoice.getSelectedIndex() == 3) {
+            } else if(xchoice.getSelectedIndex() == 3) {
                 alignTo.x += bounds.minx;
-            } else if (xchoice.getSelectedIndex() == 4) {
+            } else if(xchoice.getSelectedIndex() == 4) {
                 alignTo.x += orig.x;
             }
-            if (!Double.isNaN(vfy.getValue())) {
+            if(!Double.isNaN(vfy.getValue())) {
                 alignTo.y += vfy.getValue();
-            } else if (ychoice.getSelectedIndex() == 1) {
+            } else if(ychoice.getSelectedIndex() == 1) {
                 alignTo.y += bounds.maxy;
-            } else if (ychoice.getSelectedIndex() == 2) {
+            } else if(ychoice.getSelectedIndex() == 2) {
                 alignTo.y += center.y;
-            } else if (ychoice.getSelectedIndex() == 3) {
+            } else if(ychoice.getSelectedIndex() == 3) {
                 alignTo.y += bounds.miny;
-            } else if (ychoice.getSelectedIndex() == 4) {
+            } else if(ychoice.getSelectedIndex() == 4) {
                 alignTo.y += orig.y;
             }
-            if (!Double.isNaN(vfz.getValue())) {
+            if(!Double.isNaN(vfz.getValue())) {
                 alignTo.z += vfz.getValue();
-            } else if (zchoice.getSelectedIndex() == 1) {
+            } else if(zchoice.getSelectedIndex() == 1) {
                 alignTo.z += bounds.maxz;
-            } else if (zchoice.getSelectedIndex() == 2) {
+            } else if(zchoice.getSelectedIndex() == 2) {
                 alignTo.z += center.z;
-            } else if (zchoice.getSelectedIndex() == 3) {
+            } else if(zchoice.getSelectedIndex() == 3) {
                 alignTo.z += bounds.minz;
-            } else if (zchoice.getSelectedIndex() == 4) {
+            } else if(zchoice.getSelectedIndex() == 4) {
                 alignTo.z += orig.z;
             }
         }
         alignTo.scale(1.0 / sel.length);
 
         // Now transform all of the objects.
-        for (int i = 0; i < sel.length; i++) {
-            obj = theScene.getObject(sel[i]).getObject();
-            coords = theScene.getObject(sel[i]).getCoords();
+        for(int j: sel) {
+            obj = theScene.getObject(j).getObject();
+            coords = theScene.getObject(j).getCoords();
             bounds = obj.getBounds();
             bounds = bounds.transformAndOutset(coords.fromLocal());
             center = bounds.getCenter();
             orig = coords.getOrigin();
             undo.addCommand(UndoRecord.COPY_COORDS, coords, coords.duplicate());
-            if (xchoice.getSelectedIndex() == 1) {
+            if(xchoice.getSelectedIndex() == 1) {
                 orig.x += alignTo.x - bounds.maxx;
-            } else if (xchoice.getSelectedIndex() == 2) {
+            } else if(xchoice.getSelectedIndex() == 2) {
                 orig.x += alignTo.x - center.x;
-            } else if (xchoice.getSelectedIndex() == 3) {
+            } else if(xchoice.getSelectedIndex() == 3) {
                 orig.x += alignTo.x - bounds.minx;
-            } else if (xchoice.getSelectedIndex() == 4) {
+            } else if(xchoice.getSelectedIndex() == 4) {
                 orig.x += alignTo.x - orig.x;
             }
-            if (ychoice.getSelectedIndex() == 1) {
+            if(ychoice.getSelectedIndex() == 1) {
                 orig.y += alignTo.y - bounds.maxy;
-            } else if (ychoice.getSelectedIndex() == 2) {
+            } else if(ychoice.getSelectedIndex() == 2) {
                 orig.y += alignTo.y - center.y;
-            } else if (ychoice.getSelectedIndex() == 3) {
+            } else if(ychoice.getSelectedIndex() == 3) {
                 orig.y += alignTo.y - bounds.miny;
-            } else if (ychoice.getSelectedIndex() == 4) {
+            } else if(ychoice.getSelectedIndex() == 4) {
                 orig.y += alignTo.y - orig.y;
             }
-            if (zchoice.getSelectedIndex() == 1) {
+            if(zchoice.getSelectedIndex() == 1) {
                 orig.z += alignTo.z - bounds.maxz;
-            } else if (zchoice.getSelectedIndex() == 2) {
+            } else if(zchoice.getSelectedIndex() == 2) {
                 orig.z += alignTo.z - center.z;
-            } else if (zchoice.getSelectedIndex() == 3) {
+            } else if(zchoice.getSelectedIndex() == 3) {
                 orig.z += alignTo.z - bounds.minz;
-            } else if (zchoice.getSelectedIndex() == 4) {
+            } else if(zchoice.getSelectedIndex() == 4) {
                 orig.z += alignTo.z - orig.z;
             }
             coords.setOrigin(orig);
@@ -650,12 +650,12 @@ public class CSGEditorWindow extends ObjectEditorWindow {
             return;
         }
 
-        ArrayList<ObjectInfo> selection = new ArrayList<>();
+        List<ObjectInfo> selection = new ArrayList<>();
 
         // This did not work if only object 1 was selected. Strange bug.
         //for (int s : selected)
-        for (int s = 0; s < selected.length; s++) {
-            selection.add(theScene.getObject(selected[s]));
+        for(int i: selected) {
+            selection.add(theScene.getObject(i));
         }
 
         getView().fitToObjects(selection);
