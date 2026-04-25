609582aae63eefe977f4f70023af6e72743e0e14
Maksim Khramov <maxtemplaar@gmail.com>
2026-04-25 23:53:20 +0300

    More Kotlin files remove

---
+++ xml-data/src/main/java/artofillusion/plugin/History.java
@@ -0,0 +1,30 @@
+/* Copyright 2026 by Maksim Khramov
+
+   This program is free software; you can redistribute it and/or modify it under the
+   terms of the GNU General Public License as published by the Free Software
+   Foundation; either version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+   PARTICULAR PURPOSE.  See the GNU General Public License for more details. */
+
+package artofillusion.plugin;
+
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamImplicit;
+import lombok.val;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@XStreamAlias("history")
+public final class History {
+    public List<LogRecord> getRecords() {
+        return records == null ? Collections.emptyList() : records;
+    }
+
+    @XStreamImplicit
+    private List<LogRecord> records = new ArrayList<>();
+}
