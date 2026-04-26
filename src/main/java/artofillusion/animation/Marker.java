41e5e7087b5a7dca487729cae508de81a590f46f
Maksim Khramov <maxtemplaar@gmail.com>
2026-04-26 21:14:25 +0300

    Back to Java

--- src/main/java/artofillusion/animation/Marker.java
+++ src/main/java/artofillusion/animation/Marker.java
@@ -8,8 +8,32 @@
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE.  See the GNU General Public License for more details. */
-package artofillusion.animation
 
-import java.awt.Color
+package artofillusion.animation;
 
-data class Marker(var position: Double, val name: String, val color: Color)
+import java.awt.Color;
+
+public class Marker {
+
+  @Getter @Setter
+  public double position;
+
+  @Getter
+  private final String name;
+
+  @Getter
+  private final Color color;
+
+  public Marker(double position, String name, Color color) {
+    this.position = position;
+    this.name = name;
+    this.color = color;
+  }
+
+
+  @Override
+  public String toString() {
+    return "Marker(position=" + position + ", name=" + name + ", color=" + color + ")";
+  }
+
+}
