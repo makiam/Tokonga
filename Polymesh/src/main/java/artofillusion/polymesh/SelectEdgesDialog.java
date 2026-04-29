e96cb0fae0d6444b5e1a8f3ab73ce5bddf93ab40
Maksim Khramov <maxtemplaar@gmail.com>
2026-04-29 23:48:58 +0300

    Migrate back to Java. Select Edges Dialog

--- Polymesh/src/main/java/artofillusion/polymesh/SelectEdgesDialog.java
+++ Polymesh/src/main/java/artofillusion/polymesh/SelectEdgesDialog.java
@@ -1,5 +1,5 @@
 /*
-   Copyright (C) 2024 Maksim Khramov
+   Copyright (C) 2024-2026 Maksim Khramov
    This program is free software; you can redistribute it and/or modify it under the
    terms of the GNU General Public License as published by the Free Software
    Foundation; either version 2 of the License, or (at your option) any later version.
@@ -8,19 +8,14 @@
    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
    PARTICULAR PURPOSE.  See the GNU General Public License for more details. */
 
-package artofillusion.polymesh
+package artofillusion.polymesh;
 
-import java.util.function.Consumer
+import java.util.function.Consumer;
 
-class SelectEdgesDialog(view: PolyMeshEditorWindow, onCommit: Consumer<Int?>?) : DivideDialog(view, onCommit) {
-    /**
-     * Creates new form DivideDialog
-     *
-     * @param view
-     * @param onCommit
-     */
-    init {
-        this.setTitle("polymesh:edgesRingTitle")
-        this.setValueSpinnerLabelText("polymesh:edgesRingLabel")
+public class SelectEdgesDialog extends DivideDialog {
+    public SelectEdgesDialog(PolyMeshEditorWindow view, Consumer<Integer> onCommit) {
+        super(view, onCommit);
+        this.setTitle("polymesh:edgesRingTitle");
+        this.setValueSpinnerLabelText("polymesh:edgesRingLabel");
     }
 }
