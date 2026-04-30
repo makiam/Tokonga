0f968d861d15abdecf19e0e2a7f67d2f960b9490
Maksim Khramov <maxtemplaar@gmail.com>
2026-04-30 23:14:38 +0300

    Migrate back to Java. More Layout menus

---
+++ ArtOfIllusion/src/main/java/artofillusion/LayoutFileMenu.java
@@ -0,0 +1,46 @@
+/* Copyright 2024-2026 by Maksim Khramov
+
+   This program is free software; you can redistribute it and/or modify it under the
+   terms of the GNU General Public License as published by the Free Software
+   Foundation; either version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+   PARTICULAR PURPOSE.  See the GNU General Public License for more details. */
+
+
+package artofillusion;
+
+import artofillusion.ui.Translate;
+
+import buoy.widget.BMenu;
+import lombok.extern.slf4j.Slf4j;
+import org.greenrobot.eventbus.Subscribe;
+
+import javax.swing.*;
+
+@Slf4j
+public final class LayoutFileMenu extends BMenu {
+
+    private final LayoutWindow layout;
+
+    LayoutFileMenu(LayoutWindow layout) {
+        super(Translate.text("menu.file"));
+        this.layout = layout;
+        org.greenrobot.eventbus.EventBus.getDefault().register(this);
+
+        this.add(Translate.menuItem("new", event -> {
+            layout.savePreferences();
+            SwingUtilities.invokeLater(ArtOfIllusion::newWindow);
+        }));
+        this.add(Translate.menuItem("open", event -> {
+            layout.savePreferences();
+            SwingUtilities.invokeLater(() -> ArtOfIllusion.openScene(layout));
+        }));
+    }
+
+    @Subscribe
+    public void onSceneChangedEvent(SceneChangedEvent event) {
+        log.info("On Scene changed {} ", layout == event.getWindow() ? "this" : "other");
+    }
+}
